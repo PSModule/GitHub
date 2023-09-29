240ed803db0fa0da1d89f5209baf1dfe77f52491
Marius Storhaug <marstor@hotmail.com>
2023-09-29 19:17:01 +0200

    Add function to get all apps installed in an org

---
+++ src/GitHub/public/Organization/Get-GitHubOrganizationAppInstallation.ps1
@@ -0,0 +1,38 @@
+﻿function Get-GitHubOrganizationAppInstallation {
+    <#
+        .SYNOPSIS
+        List app installations for an organization
+
+        .DESCRIPTION
+        Lists all GitHub Apps in an organization. The installation count includes all GitHub Apps installed on repositories in the organization. You must be an organization owner with `admin:read` scope to use this endpoint.
+
+        .EXAMPLE
+        Get-GitHubOrganizationAppInstallation -OrganizationName 'github'
+
+        Gets all GitHub Apps in the organization `github`.
+
+        .NOTES
+        https://docs.github.com/rest/orgs/orgs#list-app-installations-for-an-organization
+
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param (
+        # The organization name. The name is not case sensitive.
+        [Parameter(Mandatory)]
+        [Alias('org')]
+        [string] $OrganizationName,
+
+        # The number of results per page (max 100).
+        [Parameter()]
+        [int] $PerPage = 30
+    )
+
+    $inputObject = @{
+        APIEndpoint = "/orgs/$OrganizationName/installations"
+        Method      = 'GET'
+    }
+
+    Invoke-GitHubAPI @inputObject
+
+}
