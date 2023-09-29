3869736f2c47c964b98750ced54e50ec98551dfc
Marius Storhaug <marstor@hotmail.com>
2023-09-29 17:51:45 +0200

    update testing files

--- tools/utilities/GitHubAPI.ps1
+++ tools/utilities/GitHubAPI.ps1
@@ -4,17 +4,25 @@ $Dereferenced = 'descriptions/api.github.com/dereferenced/api.github.com.deref.j
 $APIDocURI = $APIDocURI + $Bundled
 $response = Invoke-RestMethod -Uri $APIDocURI -Method Get
 
-# $response.info          # API name = GitHub REST API
-# $response.openapi       # Spec version = 3.0.3
-# $response.servers       # API URL = api.github.com
-# $response.externalDocs  # API docs URL = docs.github.com/rest
-# $response.components    # Type specs
-# $response.paths         # API endpoints
-# $response.tags          # API categories
-# $response.'x-webhooks'  # Webhooks/event docs
+$response.info          # API name = GitHub REST API
+$response.openapi       # Spec version = 3.0.3
+$response.servers       # API URL = api.github.com
+$response.externalDocs  # API docs URL = docs.github.com/rest
+$response.components    # Type specs
+$response.paths         # API endpoints # -> Namespaces, PascalCase!
+$response.tags          # API categories
+$response.'x-webhooks'  # Webhooks/event docs
+
+$response.paths.psobject.Properties | Select-Object `
+    Name, `
+    @{n = 'Get'; e = { (($_.value.psobject.Properties.Name) -contains 'Get') } }, `
+    @{n = 'Post'; e = { (($_.value.psobject.Properties.Name) -contains 'Post') } }, `
+    @{n = 'Delete'; e = { (($_.value.psobject.Properties.Name) -contains 'Delete') } }, `
+    @{n = 'PUT'; e = { (($_.value.psobject.Properties.Name) -contains 'PUT') } }, `
+    @{n = 'PATCH'; e = { (($_.value.psobject.Properties.Name) -contains 'PATCH') } } | format-table
 
 $path = '/users/{username}/hovercard'
-$response.paths.$path.get.tags | clip                            # -> Namespace/foldername
+$response.paths.$path.get.tags | clip                             # -> Namespace/foldername
 $response.paths.$path.get.operationId | clip                      # -> FunctionName
 $response.paths.$path.get.summary | clip                          # -> Synopsis
 $response.paths.$path.get.description | clip                      # -> Description
@@ -27,4 +35,5 @@ $response.paths.$path.get.parameters                              # -> Parameter
 $response.paths.$path.get.responses.'200'.content.'application/json'.schema        # -> OutputType qualifyer
 $response.paths.$path.get.responses.'200'.content.'application/json'.schema.items  # -> OutputType
 $response.paths.$path.get
+
 
