4bf519c5c99caab54a3c0e513e199aace1ef99ff
Marius Storhaug <marstor@hotmail.com>
2023-10-03 22:41:47 +0200

    Add and remove socials for the authenticated user

---
+++ src/GitHub/public/Users/Remove-GitHubUserSocials.ps1
@@ -0,0 +1,37 @@
+﻿filter Remove-GitHubUserSocials {
+    <#
+        .SYNOPSIS
+        Delete social accounts for the authenticated user
+
+        .DESCRIPTION
+        Deletes one or more social accounts from the authenticated user's profile. This endpoint is accessible with the `user` scope.
+
+        .PARAMETER AccountUrls
+        Parameter description
+
+        .EXAMPLE
+        Remove-GitHubUserSocials -AccountUrls 'https://twitter.com/MyTwitterAccount'
+
+        .NOTES
+        https://docs.github.com/rest/users/social-accounts#delete-social-accounts-for-the-authenticated-user
+    #>
+    [OutputType([void])]
+    [CmdletBinding()]
+    param (
+        # Full URLs for the social media profiles to add.
+        [Parameter(Mandatory)]
+        [Alias('account_urls')]
+        [string[]] $AccountUrls
+    )
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+
+    $inputObject = @{
+        APIEndpoint = '/user/social_accounts'
+        Body        = $body
+        Method      = 'DELETE'
+    }
+
+    (Invoke-GitHubAPI @inputObject).Response
+
+}
