840ef7c6656eb4999cff6568a5f381be41ea7dcc
Marius Storhaug <marstor@hotmail.com>
2023-10-07 14:47:53 +0200

    Added SSH key management functions

--- src/GitHub/private/Users/GPG-Keys/Get-GitHubUserMyGpgKeyById.ps1
+++ src/GitHub/private/Users/GPG-Keys/Get-GitHubUserMyGpgKeyById.ps1
@@ -8,9 +8,9 @@
         Requires that you are authenticated via Basic Auth or via OAuth with at least `read:gpg_key` [scope](https://docs.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/).
 
         .EXAMPLE
-        Get-GitHubUserMyGpgKeyById -GpgKeyID '4AEE18F83AFDEB23'
+        Get-GitHubUserMyGpgKeyById -ID '1234567'
 
-        Gets the GPG key with ID '4AEE18F83AFDEB23' for the authenticated user.
+        Gets the GPG key with ID '1234567' for the authenticated user.
 
         .NOTES
         https://docs.github.com/rest/users/gpg-keys#get-a-gpg-key-for-the-authenticated-user
---
+++ src/GitHub/private/Users/Keys/Get-GitHubUserKeyForUser.ps1
@@ -0,0 +1,44 @@
+﻿filter Get-GitHubUserKeyForUser {
+    <#
+        .SYNOPSIS
+        List public SSH keys for a user
+
+        .DESCRIPTION
+        Lists the _verified_ public SSH keys for a user. This is accessible by anyone.
+
+        .EXAMPLE
+        Get-GitHubUserKeyForUser -Username 'octocat'
+
+        Gets all public SSH keys for the 'octocat' user.
+
+        .NOTES
+        https://docs.github.com/rest/users/keys#list-public-keys-for-a-user
+
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param (
+        # The handle for the GitHub user account.
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName
+        )]
+        [string] $Username,
+
+        # The number of results per page (max 100).
+        [Parameter()]
+        [int] $PerPage = 30
+    )
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+    Remove-HashtableEntries -Hashtable $body -RemoveNames 'username'
+
+    $inputObject = @{
+        APIEndpoint = "/users/$Username/keys"
+        Method      = 'GET'
+        Body        = $body
+    }
+
+    (Invoke-GitHubAPI @inputObject).Response
+
+}
