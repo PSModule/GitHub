e0eade36e500d6fc7f8783b0fad22197a3cafe70
Marius Storhaug <marstor@hotmail.com>
2023-10-07 11:43:42 +0200

    Added Get-GitHubUserFollowing

---
+++ src/GitHub/public/Users/Followers/Get-GitHubUserFollowing.ps1
@@ -0,0 +1,36 @@
+﻿filter Get-GitHubUserFollowing {
+    <#
+        .SYNOPSIS
+        List the people the authenticated user follows
+
+        .DESCRIPTION
+        Lists the people who the authenticated user follows.
+
+        .EXAMPLE
+        Get-GitHubUserFollowing
+
+        Gets all people the authenticated user follows.
+
+        .NOTES
+        https://docs.github.com/rest/users/followers#list-the-people-the-authenticated-user-follows
+
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param (
+        # The number of results per page (max 100).
+        [Parameter()]
+        [int] $PerPage = 30
+    )
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+
+    $inputObject = @{
+        APIEndpoint = '/user/following'
+        Method      = 'GET'
+        Body        = $body
+    }
+
+    (Invoke-GitHubAPI @inputObject).Response
+
+}
--- tools/utilities/GitHubAPI.ps1
+++ tools/utilities/GitHubAPI.ps1
@@ -21,7 +21,7 @@ $response = Invoke-RestMethod -Uri $APIDocURI -Method Get
 # @{n = 'PUT'; e = { (($_.value.psobject.Properties.Name) -contains 'PUT') } }, `
 # @{n = 'PATCH'; e = { (($_.value.psobject.Properties.Name) -contains 'PATCH') } } | Format-Table
 
-$path = '/user/followers'
+$path = '/user/following'
 $method = 'GET'
 $response.paths.$path.$method
 $response.paths.$path.$method.tags | clip                             # -> Namespace/foldername
