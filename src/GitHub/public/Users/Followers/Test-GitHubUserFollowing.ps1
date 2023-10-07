c1a875f3595aeae01e2ef0b2f1d50eec2479fbc8
Marius Storhaug <marstor@hotmail.com>
2023-10-07 11:54:35 +0200

    Added Test-GitHubUserFollowing

---
+++ src/GitHub/public/Users/Followers/Test-GitHubUserFollowing.ps1
@@ -0,0 +1,45 @@
+﻿filter Test-GitHubUserFollowing {
+    <#
+        .SYNOPSIS
+        Check if a person is followed by the authenticated user
+
+        .DESCRIPTION
+        Returns a 204 if the given user is followed by the authenticated user.
+        Returns a 404 if the user is not followed by the authenticated user.
+
+        .EXAMPLE
+        Test-GitHubUserFollowing -Username 'octocat'
+
+        Checks if the authenticated user follows the user 'octocat'.
+
+        .NOTES
+        https://docs.github.com/rest/users/followers#check-if-a-person-is-followed-by-the-authenticated-user
+
+    #>
+    [OutputType([bool])]
+    [CmdletBinding()]
+    param (
+        # The handle for the GitHub user account.
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName
+        )]
+        [string] $Username
+    )
+
+    $inputObject = @{
+        APIEndpoint = "/user/following/$Username"
+        Method      = 'GET'
+    }
+
+    try {
+        $null = (Invoke-GitHubAPI @inputObject)
+        return $true
+    } catch {
+        if ($_.Exception.Response.StatusCode.Value__ -eq 404) {
+            return $false
+        } else {
+            throw $_
+        }
+    }
+}
