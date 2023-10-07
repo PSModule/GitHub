41c7b99288d5d037fbb6c836a9ad26710077d843
Marius Storhaug <marstor@hotmail.com>
2023-10-07 19:40:38 +0200

    [Feature] Releases - Releases - Manage releases (#67)
    - Fixes #58
      - Get-GitHubRelease
      - New-GitHubRelease
      - New-GitHubReleaseNotes (Generate-GitHubReleaseNotes)
      - Remove-GitHubRelease
      - Set-GitHubRelease

---
+++ src/GitHub/public/Releases/Releases/Remove-GitHubRelease.ps1
@@ -0,0 +1,44 @@
+﻿filter Remove-GitHubRelease {
+    <#
+        .SYNOPSIS
+        Delete a release
+
+        .DESCRIPTION
+        Users with push access to the repository can delete a release.
+
+        .EXAMPLE
+        Remove-GitHubRelease -Owner 'octocat' -Repo 'hello-world' -ID '1234567'
+
+        Deletes the release with the ID '1234567' for the repository 'octocat/hello-world'.
+
+        .NOTES
+        https://docs.github.com/rest/releases/releases#delete-a-release
+
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param (
+        # The account owner of the repository. The name is not case sensitive.
+        [Parameter()]
+        [string] $Owner = (Get-GitHubConfig -Name Owner),
+
+        # The name of the repository without the .git extension. The name is not case sensitive.
+        [Parameter()]
+        [string] $Repo = (Get-GitHubConfig -Name Repo),
+
+        # The unique identifier of the release.
+        [Parameter(
+            Mandatory
+        )]
+        [Alias('release_id')]
+        [string] $ID
+    )
+
+    $inputObject = @{
+        APIEndpoint = "/repos/$Owner/$Repo/releases/$ID"
+        Method      = 'DELETE'
+    }
+
+    (Invoke-GitHubAPI @inputObject).Response
+
+}
