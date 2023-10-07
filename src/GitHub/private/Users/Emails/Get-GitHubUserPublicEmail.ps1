ef475ce3a61b54e2f567d3bbb66b1268911a165a
Marius Storhaug <marstor@hotmail.com>
2023-10-07 11:25:22 +0200

    Added Get-GitHubUserPublicEmail

---
+++ src/GitHub/private/Users/Emails/Get-GitHubUserPublicEmail.ps1
@@ -0,0 +1,36 @@
+﻿filter Get-GitHubUserPublicEmail {
+    <#
+        .SYNOPSIS
+        List public email addresses for the authenticated user
+
+        .DESCRIPTION
+        Lists your publicly visible email address, which you can set with the [Set primary email visibility for the authenticated user](https://docs.github.com/rest/users/emails#set-primary-email-visibility-for-the-authenticated-user) endpoint. This endpoint is accessible with the `user:email` scope.
+
+        .EXAMPLE
+        Get-GitHubUserPublicEmail
+
+        Gets all public email addresses for the authenticated user.
+
+        .NOTES
+        https://docs.github.com/rest/users/emails#list-public-email-addresses-for-the-authenticated-user
+
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param (
+        # The number of results per page (max 100).
+        [Parameter()]
+        [int] $PerPage = 30
+    )
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+
+    $inputObject = @{
+        APIEndpoint = "/user/public_emails"
+        Method      = 'GET'
+        Body        = $body
+    }
+
+    (Invoke-GitHubAPI @inputObject).Response
+
+}
