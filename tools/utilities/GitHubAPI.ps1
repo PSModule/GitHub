84859f39981b16681258177003ad5354623b8cc6
Marius Storhaug <marstor@hotmail.com>
2023-10-07 14:09:45 +0200

    Add RemoveGitHubUserGpgKey

--- tools/utilities/GitHubAPI.ps1
+++ tools/utilities/GitHubAPI.ps1
@@ -22,7 +22,7 @@ $response = Invoke-RestMethod -Uri $APIDocURI -Method Get
 # @{n = 'PATCH'; e = { (($_.value.psobject.Properties.Name) -contains 'PATCH') } } | Format-Table
 
 $path = '/user/gpg_keys/{gpg_key_id}'
-$method = 'GET'
+$method = 'DELETE'
 $response.paths.$path.$method
 $response.paths.$path.$method.tags | clip                             # -> Namespace/foldername
 $response.paths.$path.$method.operationId | clip                      # -> FunctionName
