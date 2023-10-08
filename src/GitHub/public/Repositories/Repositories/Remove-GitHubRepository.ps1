e739cefd76d66af3c9a94d4d126f176c2a99c63d
Marius Storhaug <marstor@hotmail.com>
2023-10-08 20:57:51 +0200

    added remove repo function

---
+++ src/GitHub/public/Repositories/Repositories/Remove-GitHubRepository.ps1
@@ -0,0 +1,43 @@
+﻿filter Remove-GitHubRepository {
+    <#
+        .SYNOPSIS
+        Delete a repository
+
+        .DESCRIPTION
+        Deleting a repository requires admin access. If OAuth is used, the `delete_repo` scope is required.
+
+        If an organization owner has configured the organization to prevent members from deleting organization-owned
+        repositories, you will get a `403 Forbidden` response.
+
+        .EXAMPLE
+        Remove-GitHubRepository -Owner 'PSModule' -Repo 'Hello-World'
+
+        Deletes the repository `Hello-World` in the `PSModule` organization.
+
+        .NOTES
+        https://docs.github.com/rest/repos/repos#delete-a-repository
+
+    #>
+    [CmdletBinding()]
+    param (
+        # The account owner of the repository. The name is not case sensitive.
+        [Parameter(Mandatory)]
+        [Alias('org')]
+        [Alias('login')]
+        [string] $Owner,
+
+        # The name of the repository without the .git extension. The name is not case sensitive.
+        [Parameter(Mandatory)]
+        [string] $Repo
+    )
+
+    $inputObject = @{
+        APIEndpoint = "/repos/$Owner/$Repo"
+        Method      = 'DELETE'
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response
+    }
+
+}
