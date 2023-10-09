ac30a8197245d4085e1490c906cad69a91f23f36
Marius Storhaug <marstor@hotmail.com>
2023-10-09 23:41:56 +0200

    Added get repotags

---
+++ src/GitHub/public/Repositories/Repositories/Get-GitHubRepositoryTags.ps1
@@ -0,0 +1,47 @@
+﻿filter Get-GitHubRepositoryTags {
+    <#
+        .SYNOPSIS
+        List repository tags
+
+        .DESCRIPTION
+        List repository tags
+
+        .EXAMPLE
+        Get-GitHubRepositoryTags -Owner 'PSModule' -Repo 'GitHub'
+
+        Gets all tags of the GitHub repository.
+
+        .NOTES
+        https://docs.github.com/rest/repos/repos#list-repository-tags
+
+    #>
+    [CmdletBinding()]
+    param (
+        # The account owner of the repository. The name is not case sensitive.
+        [Parameter()]
+        [Alias('org')]
+        [string] $Owner = (Get-GitHubConfig -Name Owner),
+
+        # The name of the repository without the .git extension. The name is not case sensitive.
+        [Parameter()]
+        [string] $Repo = (Get-GitHubConfig -Name Repo),
+
+        # The number of results per page (max 100).
+        [Parameter()]
+        [ValidateRange(1, 100)]
+        [int] $PerPage = 30
+    )
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+    Remove-HashtableEntries -Hashtable $body -RemoveNames 'Owner', 'Repo' -RemoveTypes 'SwitchParameter'
+
+    $inputObject = @{
+        APIEndpoint = "/repos/$Owner/$Repo/tags"
+        Method      = 'GET'
+        Body        = $body
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response
+    }
+}
