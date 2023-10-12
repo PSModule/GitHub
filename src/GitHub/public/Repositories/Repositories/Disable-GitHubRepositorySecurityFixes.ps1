46e7bd6fe799d97fa1671f357682fa78030b7e4e
Marius Storhaug <marstor@hotmail.com>
2023-10-12 07:48:23 +0200

    Added Security fix enable disable function

---
+++ src/GitHub/public/Repositories/Repositories/Disable-GitHubRepositorySecurityFixes.ps1
@@ -0,0 +1,57 @@
+﻿filter Disable-GitHubRepositorySecurityFixes {
+    <#
+        .SYNOPSIS
+        Disable automated security fixes
+
+        .DESCRIPTION
+        Disables automated security fixes for a repository. The authenticated user must have admin access to the repository. For more information, see "[Configuring automated security fixes](https://docs.github.com/articles/configuring-automated-security-fixes)".
+
+        .EXAMPLE
+        Disable-GitHubRepositorySecurityFixes -Owner 'PSModule' -Repo 'GitHub'
+
+        Disables automated security fixes for the repository.
+
+        .NOTES
+        https://docs.github.com/rest/repos/repos#disable-automated-security-fixes
+
+    #>
+    [CmdletBinding()]
+    param (
+        # The account owner of the repository. The name is not case sensitive.
+        [Parameter()]
+        [Alias('org')]
+        [string] $Owner = (Get-GitHubConfig -Name Owner),
+
+        # The name of the repository without the .git extension. The name is not case sensitive.
+        [Parameter()]
+        [string] $Repo = (Get-GitHubConfig -Name Repo)
+    )
+
+    $PSCmdlet.MyInvocation.MyCommand.Parameters.GetEnumerator() | ForEach-Object {
+        $paramName = $_.Key
+        $paramDefaultValue = Get-Variable -Name $paramName -ValueOnly -ErrorAction SilentlyContinue
+        $providedValue = $PSBoundParameters[$paramName]
+        Write-Verbose "[$paramName]"
+        Write-Verbose "  - Default:  [$paramDefaultValue]"
+        Write-Verbose "  - Provided: [$providedValue]"
+        if (-not $PSBoundParameters.ContainsKey($paramName) -and ($null -ne $paramDefaultValue)) {
+            Write-Verbose '  - Using default value'
+            $PSBoundParameters[$paramName] = $paramDefaultValue
+        } else {
+            Write-Verbose '  - Using provided value'
+        }
+    }
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+    Remove-HashtableEntries -Hashtable $body -RemoveNames 'Owner', 'Repo' -RemoveTypes 'SwitchParameter'
+
+    $inputObject = @{
+        APIEndpoint = "/repos/$Owner/$Repo/automated-security-fixes"
+        Method      = 'DELETE'
+        Body        = $body
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response.names
+    }
+}
---
+++ src/GitHub/public/Repositories/Repositories/Enable-GitHubRepositorySecurityFixes.ps1
@@ -0,0 +1,57 @@
+﻿filter Enable-GitHubRepositorySecurityFixes {
+    <#
+        .SYNOPSIS
+        Enable automated security fixes
+
+        .DESCRIPTION
+        Enables automated security fixes for a repository. The authenticated user must have admin access to the repository. For more information, see "[Configuring automated security fixes](https://docs.github.com/articles/configuring-automated-security-fixes)".
+
+        .EXAMPLE
+        Enable-GitHubRepositorySecurityFixes -Owner 'PSModule' -Repo 'GitHub'
+
+        Enables automated security fixes for the repository.
+
+        .NOTES
+        https://docs.github.com/rest/repos/repos#enable-automated-security-fixes
+
+    #>
+    [CmdletBinding()]
+    param (
+        # The account owner of the repository. The name is not case sensitive.
+        [Parameter()]
+        [Alias('org')]
+        [string] $Owner = (Get-GitHubConfig -Name Owner),
+
+        # The name of the repository without the .git extension. The name is not case sensitive.
+        [Parameter()]
+        [string] $Repo = (Get-GitHubConfig -Name Repo)
+    )
+
+    $PSCmdlet.MyInvocation.MyCommand.Parameters.GetEnumerator() | ForEach-Object {
+        $paramName = $_.Key
+        $paramDefaultValue = Get-Variable -Name $paramName -ValueOnly -ErrorAction SilentlyContinue
+        $providedValue = $PSBoundParameters[$paramName]
+        Write-Verbose "[$paramName]"
+        Write-Verbose "  - Default:  [$paramDefaultValue]"
+        Write-Verbose "  - Provided: [$providedValue]"
+        if (-not $PSBoundParameters.ContainsKey($paramName) -and ($null -ne $paramDefaultValue)) {
+            Write-Verbose '  - Using default value'
+            $PSBoundParameters[$paramName] = $paramDefaultValue
+        } else {
+            Write-Verbose '  - Using provided value'
+        }
+    }
+
+    $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
+    Remove-HashtableEntries -Hashtable $body -RemoveNames 'Owner', 'Repo' -RemoveTypes 'SwitchParameter'
+
+    $inputObject = @{
+        APIEndpoint = "/repos/$Owner/$Repo/automated-security-fixes"
+        Method      = 'PUT'
+        Body        = $body
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response.names
+    }
+}
