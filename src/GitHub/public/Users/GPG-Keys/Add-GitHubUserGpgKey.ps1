6a6a8c28235698b3bc5e4970557f036ff770bcfe
Marius Storhaug <marstor@hotmail.com>
2023-10-28 00:47:50 +0200

    fix

--- src/GitHub/public/Users/GPG-Keys/Add-GitHubUserGpgKey.ps1
+++ src/GitHub/public/Users/GPG-Keys/Add-GitHubUserGpgKey.ps1
@@ -5,10 +5,17 @@
 
         .DESCRIPTION
         Adds a GPG key to the authenticated user's GitHub account.
-        Requires that you are authenticated via Basic Auth, or OAuth with at least `write:gpg_key` [scope](https://docs.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/).
+        Requires that you are authenticated via Basic Auth, or OAuth with at least `write:gpg_key`
+        [scope](https://docs.github.com/apps/building-oauth-apps/understanding-scopes-for-oauth-apps/).
 
         .EXAMPLE
-        Add-GitHubUserGpgKey -Name 'GPG key for GitHub' -ArmoredPublicKey '-----BEGIN PGP PUBLIC KEY BLOCK-----\nVersion: GnuPG v1\n\nmQINBFnZ2ZIBEADQ2Z7Z7\n-----END PGP PUBLIC KEY BLOCK-----'
+        Add-GitHubUserGpgKey -Name 'GPG key for GitHub' -ArmoredPublicKey @'
+        -----BEGIN PGP PUBLIC KEY BLOCK-----
+        Version: GnuPG v1
+
+        mQINBFnZ2ZIBEADQ2Z7Z7
+        -----END PGP PUBLIC KEY BLOCK-----
+        '@
 
         Adds a GPG key to the authenticated user's GitHub account.
 
@@ -40,7 +47,7 @@
     $body = $PSBoundParameters | ConvertFrom-HashTable | ConvertTo-HashTable -NameCasingStyle snake_case
 
     $inputObject = @{
-        APIEndpoint = "/user/gpg_keys"
+        APIEndpoint = '/user/gpg_keys'
         Method      = 'POST'
         Body        = $body
     }
