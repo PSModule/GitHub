e222812b8c421df36cfd8c96d1e1ab6f18ed47fc
Marius Storhaug <marstor@hotmail.com>
2023-10-31 08:17:54 +0100

    Added a remaining file

--- tools/utilities/GitHubAPI.ps1
+++ tools/utilities/GitHubAPI.ps1
@@ -21,7 +21,7 @@ $response = Invoke-RestMethod -Uri $APIDocURI -Method Get
 # @{n = 'PUT'; e = { (($_.value.psobject.Properties.Name) -contains 'PUT') } }, `
 # @{n = 'PATCH'; e = { (($_.value.psobject.Properties.Name) -contains 'PATCH') } } | Format-Table
 
-$path = '/repositories/{repository_id}/environments/{environment_name}/secrets/{secret_name}'
+$path = '/repos/{owner}/{repo}/vulnerability-alerts'
 $method = 'get'
 $response.paths.$path.$method
 $response.paths.$path.$method.tags | clip                             # -> Namespace/foldername
