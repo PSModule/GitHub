9e128823a99c90ce713989c6ebd2e4ace3a622af
Marius Storhaug <marstor@hotmail.com>
2023-11-05 11:53:44 +0100

    Add test for GitHubStatus

---
+++ tests/GitHub/Get-GitHubStatus.Tests.ps1
@@ -0,0 +1,17 @@
+﻿Describe 'Get-GitHubStatus' {
+    It 'Function exists' {
+        Get-Command Get-GitHubStatus | Should -Not -BeNullOrEmpty
+    }
+
+    Context 'Parameter Set: Default' {
+        It 'Can be called with no parameters' {
+            { Get-GitHubStatus } | Should -Not -Throw
+        }
+    }
+
+    Context 'Parameter Set: Summary' {
+        It 'Can be called with Summary parameter' {
+            { Get-GitHubStatus -Summary } | Should -Not -Throw
+        }
+    }
+}
