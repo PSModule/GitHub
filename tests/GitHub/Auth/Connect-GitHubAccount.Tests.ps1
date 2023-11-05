510278792ad89ca85c21d6870485479836e2b741
Marius Storhaug <marstor@hotmail.com>
2023-11-05 13:12:16 +0100

    test

--- tests/GitHub/Auth/Connect-GitHubAccount.Tests.ps1
+++ tests/GitHub/Auth/Connect-GitHubAccount.Tests.ps1
@@ -3,23 +3,9 @@
         Get-Command Connect-GitHubAccount | Should -Not -BeNullOrEmpty
     }
 
-    Context 'Parameter Set: DeviceFlow' {
+    Context 'Parameter Set: sPAT' {
         It 'Can be called with no parameters' {
-            { Connect-GitHubAccount } | Should -Not -Throw
-        }
-
-        It 'Can be called with Mode parameter' {
-            { Connect-GitHubAccount -Mode 'OAuthApp' } | Should -Not -Throw
-        }
-
-        It 'Can be called with Scope parameter' {
-            { Connect-GitHubAccount -Scope 'gist read:org repo workflow' } | Should -Not -Throw
-        }
-    }
-
-    Context 'Parameter Set: PAT' {
-        It 'Can be called with AccessToken parameter' {
-            { Connect-GitHubAccount -AccessToken } | Should -Not -Throw
+            { Connect-GitHubAccount -Verbose } | Should -Not -Throw
         }
     }
 }
