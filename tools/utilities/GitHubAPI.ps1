bc32cb7d2c589f035c1c728d2b5d16c7a57824ab
Marius Storhaug <marstor@hotmail.com>
2023-11-05 21:06:36 +0100

    Get forks

--- tools/utilities/GitHubAPI.ps1
+++ tools/utilities/GitHubAPI.ps1
@@ -22,7 +22,7 @@ $response = Invoke-RestMethod -Uri $APIDocURI -Method Get
 # @{n = 'PATCH'; e = { (($_.value.psobject.Properties.Name) -contains 'PATCH') } } | Format-Table
 
 $path = '/repos/{owner}/{repo}/forks'
-$method = 'post'
+$method = 'get'
 $response.paths.$path.$method
 $response.paths.$path.$method.tags | clip                             # -> Namespace/foldername
 $response.paths.$path.$method.operationId | clip                      # -> FunctionName
