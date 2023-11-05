dcd62135ae455cd8be30f5a7cd588c4f6bfa2272
Marius Storhaug <marstor@hotmail.com>
2023-11-05 13:06:25 +0100

    Added Auth tests

---
+++ tests/GitHub/Auth/Connect-GitHubAccount.Tests.ps1
@@ -0,0 +1,25 @@
+﻿Describe 'Connect-GitHubAccount' {
+    It 'Function exists' {
+        Get-Command Connect-GitHubAccount | Should -Not -BeNullOrEmpty
+    }
+
+    Context 'Parameter Set: DeviceFlow' {
+        It 'Can be called with no parameters' {
+            { Connect-GitHubAccount } | Should -Not -Throw
+        }
+
+        It 'Can be called with Mode parameter' {
+            { Connect-GitHubAccount -Mode 'OAuthApp' } | Should -Not -Throw
+        }
+
+        It 'Can be called with Scope parameter' {
+            { Connect-GitHubAccount -Scope 'gist read:org repo workflow' } | Should -Not -Throw
+        }
+    }
+
+    Context 'Parameter Set: PAT' {
+        It 'Can be called with AccessToken parameter' {
+            { Connect-GitHubAccount -AccessToken } | Should -Not -Throw
+        }
+    }
+}
---
+++ tests/GitHub/Auth/Disconnect-GitHubAccount.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Describe 'Disconnect-GitHubAccount' {
+    It 'Function exists' {
+        Get-Command Disconnect-GitHubAccount | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Can be called with no parameters' {
+        { Disconnect-GitHubAccount } | Should -Not -Throw
+    }
+}
