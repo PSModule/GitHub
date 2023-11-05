b88ff7b5531b2bb3d8501a88335c5bc93e71a02d
Marius Storhaug <marstor@hotmail.com>
2023-11-05 12:22:29 +0100

    Added tests for ratelimit

---
+++ tests/Rate-Limit/Get-GitHubRateLimit.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Describe 'Get-GitHubRateLimit' {
+    It 'Function exists' {
+        Get-Command Get-GitHubRateLimit | Should -Not -BeNullOrEmpty
+    }
+
+    It 'Can be called with no parameters' {
+        { Get-GitHubRateLimit } | Should -Not -Throw
+    }
+}
