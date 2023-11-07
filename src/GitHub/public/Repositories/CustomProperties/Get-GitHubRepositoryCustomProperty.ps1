7937a652594ca89e31c4e4d4d00dae60edae12ca
Marius Storhaug <marstor@hotmail.com>
2023-11-07 20:33:47 +0100

    Adds function for get repo custom props

---
+++ src/GitHub/public/Repositories/CustomProperties/Get-GitHubRepositoryCustomProperty.ps1
@@ -0,0 +1,41 @@
+﻿filter Get-GitHubRepositoryCustomProperty {
+    <#
+        .SYNOPSIS
+        Get all custom property values for a repository
+
+        .DESCRIPTION
+        Gets all custom property values that are set for a repository.
+        Users with read access to the repository can use this endpoint.
+
+        .EXAMPLE
+        Get-GitHubRepositoryCustomProperty -Owner 'octocat' -Repo 'hello-world'
+
+        Gets all custom property values that are set for the 'hello-world' repository.
+
+        .NOTES
+        https://docs.github.com/rest/repos/custom-properties#get-all-custom-property-values-for-a-repository
+
+    #>
+    [OutputType([pscustomobject])]
+    [Alias('Get-GitHubRepositoryCustomProperties')]
+    [CmdletBinding()]
+    param (
+        # The account owner of the repository. The name is not case sensitive.
+        [Parameter()]
+        [Alias('org')]
+        [string] $Owner = (Get-GitHubConfig -Name Owner),
+
+        # The name of the repository without the .git extension. The name is not case sensitive.
+        [Parameter()]
+        [string] $Repo = (Get-GitHubConfig -Name Repo)
+    )
+
+    $inputObject = @{
+        APIEndpoint = "/repos/$Owner/$Repo/properties/values"
+        Method      = 'GET'
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response
+    }
+}
