d3c65e76a6b56c302ad0c744a2696d07be6566c6
Marius Storhaug <marstor@hotmail.com>
2024-04-13 23:36:24 +0200

    🩹 [Patch]: Remove module import tests (#99)
    ## Description
    
    - Remove module import tests, as they are now default in Test-PSModule
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/GitHub.Tests.ps1
+++ tests/GitHub.Tests.ps1
@@ -8,13 +8,4 @@ Param(
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
 Describe 'GitHub' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'GitHub' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'GitHub' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be imported' {
-            { Import-Module -Name 'GitHub' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
-        }
-    }
 }
