efd2716a1d000ce91b90ae2d8ac01dc351f29f0a
Marius Storhaug <marstor@hotmail.com>
2024-11-04 22:27:38 +0100

    🩹 [Patch]: Add functions to manage GraphQLQuery and Installation Access Tokens (IATs) (#132)
    ## Description
    
    Added functions to:
    - get GitHub App installation access tokens
    - get organizations in a GitHub Enterprise instance
    - invoke GraphQL queries
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/functions/public/GraphQL/Invoke-GitHubGraphQLQuery.ps1
@@ -0,0 +1,35 @@
+﻿function Invoke-GitHubGraphQLQuery {
+    <#
+        .SYNOPSIS
+        Invoke a GraphQL query against the GitHub GraphQL API
+
+        .DESCRIPTION
+        Use this function to invoke a GraphQL query against the GitHub GraphQL API.
+
+        .EXAMPLE
+        Invoke-GitHubGraphQLQuery -Query $query -Variables $Variables
+
+        .NOTES
+        [GitHub GraphQL API documentation](https://docs.github.com/graphql)
+    #>
+    param (
+        # The GraphQL query to execute.
+        [string] $Query,
+
+        # The variables to pass to the query.
+        [hashtable] $Variables
+    )
+
+    $inputObject = @{
+        APIEndpoint = '/graphql'
+        Method      = 'Post'
+        Body        = @{
+            'query'     = $query
+            'variables' = $variables
+        } | ConvertTo-Json
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response
+    }
+}
