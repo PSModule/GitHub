38f337773e2fcfb342557cf6276dbf897a69ef14
Marius Storhaug <marstor@hotmail.com>
2024-11-04 23:45:13 +0100

    🩹 [Patch]: Adding functions to manage Github Apps on enterprise level (#133)
    ## Description
    
    - Adding functions to manage Github Apps on enterprise level.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/functions/public/Enterprise/Get-GitHubEnterpriseInstallableOrganizations.ps1
@@ -0,0 +1,29 @@
+﻿function Get-GitHubEnterpriseInstallableOrganization {
+    <#
+        .SYNOPSIS
+        Get enterprise-owned organizations that can have GitHub Apps installed
+
+        .DESCRIPTION
+        List of organizations owned by the enterprise on which the authenticated GitHub App installation may install other GitHub Apps.
+
+        The authenticated GitHub App must be installed on the enterprise and be granted the Enterprise/enterprise_organization_installations
+        (read) permission.
+
+        .EXAMPLE
+        Get-GitHubEnterpriseInstallableOrganization -Enterprise 'msx'
+    #>
+    [CmdletBinding()]
+    param(
+        # The enterprise slug or id.
+        [Parameter(Mandatory)]
+        [string] $Enterprise
+    )
+    $inputObject = @{
+        APIEndpoint = "/enterprises/$Enterprise/apps/installable_organizations"
+        Method      = 'GET'
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response
+    }
+}
