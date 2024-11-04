bb8f98462ce57591be14f29e370762c4ae147f3e
Marius Storhaug <marstor@hotmail.com>
2024-11-04 14:21:03 +0100

    🚀 [Feature]: Adding support for logging in as a GitHub App (#126)
    ## Description
    
    - Adding support for logging in as a GitHub App
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/functions/public/Apps/Get-GitHubAppJSONWebToken.ps1
@@ -0,0 +1,106 @@
+function Get-GitHubAppJSONWebToken {
+    <#
+        .SYNOPSIS
+        Generates a JSON Web Token (JWT) for a GitHub App.
+
+        .DESCRIPTION
+        Generates a JSON Web Token (JWT) for a GitHub App.
+
+        .EXAMPLE
+        Get-GitHubAppJWT -ClientId 'Iv987654321' -PrivateKeyFilePath '/path/to/private-key.pem'
+
+        Generates a JSON Web Token (JWT) for a GitHub App using the specified client ID and private key file path.
+
+        .EXAMPLE
+        Get-GitHubAppJWT -ClientId 'Iv987654321' -PrivateKey '--- BEGIN RSA PRIVATE KEY --- ... --- END RSA PRIVATE KEY ---'
+
+        Generates a JSON Web Token (JWT) for a GitHub App using the specified client ID and private key.
+
+        .OUTPUTS
+        System.String
+
+        .NOTES
+        [Generating a JSON Web Token (JWT) for a GitHub App | GitHub Docs](https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app#example-using-powershell-to-generate-a-jwt)
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSAvoidLongLines',
+        '',
+        Justification = 'Contains a long link.'
+    )]
+    [CmdletBinding(DefaultParameterSetName = 'PrivateKey')]
+    [Alias('Get-GitHubAppJWT')]
+    [OutputType([string])]
+    param(
+        # The client ID of the GitHub App.
+        # Can use the GitHub App ID or the client ID.
+        # Example: 'Iv23li8tyK9NUwl7rWlQ'
+        # Example: '123456'
+        [Parameter(Mandatory)]
+        [string] $ClientId,
+
+        # The path to the private key file of the GitHub App.
+        # Example: '/path/to/private-key.pem'
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'FilePath'
+        )]
+        [string] $PrivateKeyFilePath,
+
+        # The private key of the GitHub App.
+        # Example: @'
+        # -----BEGIN RSA PRIVATE KEY-----
+        # qwe
+        # ...
+        # -----END RSA PRIVATE KEY-----
+        # '@
+        [Parameter(
+            Mandatory,
+            ParameterSetName = 'PrivateKey'
+        )]
+        [string] $PrivateKey
+    )
+
+    if ($PrivateKeyFilePath) {
+        if (-not (Test-Path -Path $PrivateKeyFilePath)) {
+            throw "The private key path [$PrivateKeyFilePath] does not exist."
+        }
+
+        $PrivateKey = Get-Content -Path $PrivateKeyFilePath -Raw
+    }
+
+    $header = [Convert]::ToBase64String(
+        [System.Text.Encoding]::UTF8.GetBytes(
+            (
+                ConvertTo-Json -InputObject @{
+                    alg = 'RS256'
+                    typ = 'JWT'
+                }
+            )
+        )
+    ).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+
+    $payload = [Convert]::ToBase64String(
+        [System.Text.Encoding]::UTF8.GetBytes(
+            (
+                ConvertTo-Json -InputObject @{
+                    iat = [System.DateTimeOffset]::UtcNow.AddSeconds(-10).ToUnixTimeSeconds()
+                    exp = [System.DateTimeOffset]::UtcNow.AddMinutes(10).ToUnixTimeSeconds()
+                    iss = $ClientId
+                }
+            )
+        )
+    ).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+
+    $rsa = [System.Security.Cryptography.RSA]::Create()
+    $rsa.ImportFromPem($PrivateKey)
+
+    $signature = [Convert]::ToBase64String(
+        $rsa.SignData(
+            [System.Text.Encoding]::UTF8.GetBytes("$header.$payload"),
+            [System.Security.Cryptography.HashAlgorithmName]::SHA256,
+            [System.Security.Cryptography.RSASignaturePadding]::Pkcs1
+        )
+    ).TrimEnd('=').Replace('+', '-').Replace('/', '_')
+    $jwt = "$header.$payload.$signature"
+    $jwt
+}
---
+++ tools/dev/dump.ps1
@@ -0,0 +1,150 @@
+function Get-GitHubEnterpriseOrganization {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string] $Token,
+
+        [Parameter()]
+        [string] $EnterpriseSlug = 'dnb',
+
+        [Parameter()]
+        $URL = $env:GITHUB_GRAPHQL_URL
+    )
+    $headers = @{
+        'Authorization' = "Bearer $token"
+        'Content-Type'  = 'application/json'
+    }
+
+    # Define GraphQL query
+    $query = @"
+query(`$enterpriseSlug: String!, `$first: Int = 100, `$after: String) {
+  enterprise(slug: `$enterpriseSlug) {
+    organizations(first: `$first, after: `$after) {
+      edges {
+        node {
+          name
+          login
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+}
+"@
+
+    # Initialize pagination variables
+    $variables = @{
+        'enterpriseSlug' = $EnterpriseSlug
+        'first'          = 100
+        'after'          = $null
+    }
+    $allOrgs = @()
+
+    # Function to make the GraphQL request
+    function Invoke-GraphQLQuery {
+        param (
+            [string]$query,
+            [hashtable]$variables,
+            [string]$URL
+        )
+
+        $body = @{
+            'query'     = $query
+            'variables' = $variables
+        } | ConvertTo-Json
+
+        $response = Invoke-RestMethod -Uri $URL -Method Post -Headers $headers -Body $body
+        return $response
+    }
+
+    # Loop through pages to retrieve all organizations
+    do {
+        $response = Invoke-GraphQLQuery -query $query -variables $variables -URL $URL
+        # Check for errors
+        if ($response.errors) {
+            Write-Error "Error: $($response.errors[0].message)"
+            break
+        }
+
+        # Extract organization names and add to the list
+        foreach ($org in $response.data.enterprise.organizations.edges) {
+            $allOrgs += $org.node.name
+        }
+
+        # Update pagination cursor
+        $pageInfo = $response.data.enterprise.organizations.pageInfo
+        $variables.after = $pageInfo.endCursor
+
+    } while ($pageInfo.hasNextPage -eq $true)
+
+    # Output the list of organization names
+    $allOrgs | ForEach-Object { Write-Output $_ }
+
+}
+
+function Get-GitHubAppInstallation {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $Token,
+
+        [Parameter()]
+        $BaseURL = $env:GITHUB_API_URL
+    )
+
+    $result = Invoke-RestMethod -Method Get -Uri "$BaseURL/app/installations" -Headers @{
+        'Authorization' = "Bearer $token"
+        'Accept'        = 'application/vnd.github+json'
+    } -FollowRelLink
+
+    $result | ForEach-Object {
+        Write-Output $_
+    }
+}
+
+function Get-GitHubAppInstallationAccessToken {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $InstallationID,
+
+        [Parameter(Mandatory)]
+        [string] $Token,
+
+        [Parameter()]
+        $BaseURL = $env:GITHUB_API_URL
+    )
+
+    $result = Invoke-RestMethod -Method Post -Uri "$BaseURL/app/installations/$InstallationID/access_tokens" -Headers @{
+        'Authorization' = "Bearer $token"
+        'Accept'        = 'application/vnd.github+json'
+    }
+
+    $result
+}
+
+function Get-GitHubRepository {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $OrganizationName,
+
+        [Parameter(Mandatory)]
+        [string] $Token,
+
+        [Parameter()]
+        $BaseURL = $env:GITHUB_API_URL
+    )
+
+    $result = Invoke-RestMethod -Method Get -Uri "$BaseURL/orgs/$OrganizationName/repos" -Headers @{
+        'Authorization' = "Bearer $token"
+        'Accept'        = 'application/vnd.github+json'
+    } -FollowRelLink
+
+    $result | ForEach-Object {
+        Write-Output $_
+    }
+}
