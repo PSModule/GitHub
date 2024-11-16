8022f4a2c543628f94988005de537ae45d4b634d
Marius Storhaug <marstor@hotmail.com>
2024-11-16 23:23:53 +0100

    Update Context module requirement to version 2.0.4 in multiple scripts

--- src/functions/public/Auth/Context/Get-GitHubContext.ps1
+++ src/functions/public/Auth/Context/Get-GitHubContext.ps1
@@ -1,4 +1,4 @@
-﻿#Requires -Modules @{ ModuleName = 'Context'; RequiredVersion = '2.0.3' }
+﻿#Requires -Modules @{ ModuleName = 'Context'; RequiredVersion = '2.0.4' }
 
 function Get-GitHubContext {
     <#
