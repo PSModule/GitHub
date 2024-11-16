4f8248eb3ca1ed08cc93793189fbed3d8fd1ffd0
Marius Storhaug <marstor@hotmail.com>
2024-11-16 12:55:44 +0100

    Fix loading

--- src/loader.ps1
+++ src/loader.ps1
@@ -3,10 +3,6 @@ $scriptFilePath = $MyInvocation.MyCommand.Path
 
 
 Write-Verbose "[$scriptFilePath] - Initializing GitHub PowerShell module..."
-Write-Verbose ($moduleInfo.Version | Out-String)
-Write-Verbose ($moduleInfo.CommandType | Out-String)
-Write-Verbose ($moduleInfo.ModuleName | Out-String)
-Write-Verbose ($moduleInfo.Name | Out-String)
 
 if ($env:GITHUB_ACTIONS -eq 'true') {
     Write-Verbose 'Detected running on a GitHub Actions runner, preparing environment...'
@@ -19,7 +15,7 @@ if ($env:GITHUB_ACTIONS -eq 'true') {
 $context = Get-Context -Name $script:Config.Name -AsPlainText
 
 # Add values from the variables, if the values are not present in the context
-$context['Name'] = $context['Name'] ?? $script:Config.Name
+$context['Name'] = $context.Name ?? $script:Config.Name
 
 # Set the context using the values from the variables
 Set-Context @context
