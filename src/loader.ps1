6dbd20803a36d49728d63ee8b3f25291e5cd5d24
Marius Storhaug <marstor@hotmail.com>
2024-11-16 12:45:39 +0100

    Enhance loader script to re-load preexisting settings from module context

--- src/loader.ps1
+++ src/loader.ps1
@@ -3,7 +3,10 @@ $scriptFilePath = $MyInvocation.MyCommand.Path
 
 
 Write-Verbose "[$scriptFilePath] - Initializing GitHub PowerShell module..."
-Write-Verbose ($moduleInfo.Module | Out-String)
+Write-Verbose ($moduleInfo.Version | Out-String)
+Write-Verbose ($moduleInfo.CommandType | Out-String)
+Write-Verbose ($moduleInfo.ModuleName | Out-String)
+Write-Verbose ($moduleInfo.Name | Out-String)
 
 if ($env:GITHUB_ACTIONS -eq 'true') {
     Write-Verbose 'Detected running on a GitHub Actions runner, preparing environment...'
@@ -12,7 +15,11 @@ if ($env:GITHUB_ACTIONS -eq 'true') {
 }
 
 ### This is the context for this module
-Set-Context @{
-    Name           = $script:Config.Name
-    DefaultContext = 'null'
-}
+# Get current module context
+$context = Get-Context -Name $script:Config.Name -AsPlainText
+
+# Add values from the variables, if the values are not present in the context
+$context['Name'] = $context['Name'] ?? $script:Config.Name
+
+# Set the context using the values from the variables
+Set-Context @context
