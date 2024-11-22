b11eb58c4c77c00b34a35fe6bf1596b5ab23d8e7
Marius Storhaug <marstor@hotmail.com>
2024-11-22 16:01:48 +0100

    Add GitHubContext class to manage GitHub API authentication details

---
+++ src/classes/public/GitHubContext.ps1
@@ -0,0 +1,83 @@
+﻿class GitHubContext {
+    # The API base URI.
+    # https://api.github.com
+    [string] $ApiBaseUri
+
+    # The GitHub API version.
+    # 2022-11-28
+    [string] $ApiVersion
+
+    # The authentication client ID.
+    # Client ID for UAT
+    [string] $AuthClientID
+
+    # The authentication type.
+    # UAT / PAT / App / IAT
+    [string] $AuthType
+
+    # Client ID for GitHub Apps
+    [string] $ClientID
+
+    # The device flow type.
+    # GitHubApp / OAuthApp
+    [string] $DeviceFlowType
+
+    # The API hostname.
+    # github.com / msx.ghe.com / github.local
+    [string] $HostName
+
+    # User ID / App ID as GraphQL Node ID
+    [string] $NodeID
+
+    # The Database ID of the context.
+    [string] $DatabaseID
+
+    # The context ID.
+    # HostName/Username or HostName/AppSlug
+    [string] $ContextID
+
+    # The user name.
+    [string] $UserName
+
+    # The default value for the Owner parameter.
+    [string] $Owner
+
+    # The default value for the Repo parameter.
+    [string] $Repo
+
+    # The scope when authenticating with OAuth.
+    # 'gist read:org repo workflow'
+    [string] $Scope
+
+    # The token type.
+    # ghu / gho / ghp / github_pat / PEM / ghs /
+    [string] $TokenType
+
+    # The access token.
+    [securestring] $Token
+
+    # The token expiration date.
+    # 2024-01-01-00:00:00
+    [datetime] $TokenExpirationDate
+
+    # The refresh token.
+    [securestring] $RefreshToken
+
+    # The refresh token expiration date.
+    # 2024-01-01-00:00:00
+    [datetime] $RefreshTokenExpirationDate
+
+    GitHubContext([string]$ContextID) {
+        $this.ContextID = $ContextID
+    }
+
+    GitHubContext([hashtable]$Properties) {
+        $this.Init($Properties)
+    }
+
+    [void] Init([hashtable]$Properties) {
+        foreach ($Property in $Properties.Keys) {
+            $this.$Property = $Properties.$Property
+        }
+    }
+}
