7ae6dff738c0c91efa3c5b2737009ef84c1d973c
Marius Storhaug <marstor@hotmail.com>
2024-11-23 14:16:57 +0100

    Add Remove-GitHubContext function to manage context removal

---
+++ src/functions/public/Auth/Context/Remove-GitHubContext.ps1
@@ -0,0 +1,37 @@
+﻿#Requires -Modules @{ ModuleName = 'Context'; RequiredVersion = '3.0.3' }
+
+filter Remove-GitHubContext {
+    <#
+        .SYNOPSIS
+        Removes a context from the context vault.
+
+        .DESCRIPTION
+        This function removes a context from the vault. It supports removing a single context by name,
+        multiple contexts using wildcard patterns, and can also accept input from the pipeline.
+        If the specified context(s) exist, they will be removed from the vault.
+
+        .EXAMPLE
+        Remove-Context
+
+        Removes all contexts from the vault.
+
+        .EXAMPLE
+        Remove-Context -ID 'MySecret'
+
+        Removes the context called 'MySecret' from the vault.
+    #>
+    [OutputType([void])]
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        # The name of the context.
+        [Parameter(Mandatory)]
+        [Alias('Name')]
+        [string] $Context
+    )
+
+    $ID = "$($script:Config.Name)/$Context"
+
+    if ($PSCmdlet.ShouldProcess('Remove-Secret', $context.Name)) {
+        Remove-Context -ID $ID
+    }
+}
