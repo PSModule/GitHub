b83c86c0829a7b87952699c50e53b560642c7297
Marius Storhaug <marstor@hotmail.com>
2024-11-25 22:09:00 +0100

    🚀 [Feature]: Add `Get-GitHubOrganizationPendingInvitation` function to list pending organization invitations

---
+++ src/functions/public/Organization/Members/Get-GitHubOrganizationPendingInvitation.ps1
@@ -0,0 +1,58 @@
+﻿function Get-GitHubOrganizationPendingInvitation {
+    <#
+        .SYNOPSIS
+        List pending organization invitations
+
+        .DESCRIPTION
+        The return hash contains a `role` field which refers to the Organization
+        Invitation role and will be one of the following values: `direct_member`, `admin`,
+        `billing_manager`, or `hiring_manager`. If the invitee is not a GitHub
+        member, the `login` field in the return hash will be `null`.
+
+        .NOTES
+        [List pending organization invitations](https://docs.github.com/rest/orgs/members#list-pending-organization-invitations)
+    #>
+    [CmdletBinding()]
+    param (
+        # The organization name. The name is not case sensitive.
+        [Parameter(Mandatory)]
+        [Alias('Org')]
+        [string] $Organization,
+
+        # Filter invitations by their member role.
+        [Parameter()]
+        [ValidateSet('all', 'admin', 'direct_member', 'billing_manager', 'hiring_manager')]
+        [string] $Role = 'all',
+
+        # Filter invitations by their invitation source.
+        [Parameter()]
+        [ValidateSet('all', 'member', 'scim')]
+        [string] $InvitationSource = 'all',
+
+        # The number of results per page (max 100).
+        [Parameter()]
+        [ValidateRange(1, 100)]
+        [int] $PerPage = 30,
+
+        # The context to run the command in
+        [Parameter()]
+        [string] $Context = (Get-GitHubConfig -Name 'DefaultContext')
+    )
+
+    $body = @{
+        role              = $Role
+        invitation_source = $InvitationSource
+        per_page          = $PerPage
+    }
+
+    $inputObject = @{
+        Context     = $Context
+        Body        = $body
+        Method      = 'Get'
+        APIEndpoint = "/orgs/$Organization/invitations"
+    }
+
+    Invoke-GitHubAPI @inputObject | ForEach-Object {
+        Write-Output $_.Response
+    }
+}
