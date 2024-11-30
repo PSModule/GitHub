761408502705e21bab7adce663e92a0cc6b13483
Marius Storhaug <marstor@hotmail.com>
2024-11-30 16:07:51 +0100

    🩹 [Patch]: Add a command for Git config (#173)
    ## Description
    
    Introducing new functions to set and Git configuration. Now a Git config
    is set automatically when running 'Connect-GitHub' using a IAT token.
    
    - Fixes #157
    
    Function changes:
    
    - Added a new function `Get-GitHubGitConfig` to retrieve the global Git
    configuration.
    - Added a new function `Set-GitHubGitConfig` to set the Git
    configuration for a specified GitHub context. Gets details from the
    selected context and sets the git config.
    - Updated the `Set-GitHubContext` function to call `Set-GitHubGitConfig`
    when the `AuthType` is 'IAT' and running in GitHub Actions (`GHA`).
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/functions/public/Git/Get-GitHubGitConfig.ps1
@@ -0,0 +1,36 @@
+﻿function Get-GitHubGitConfig {
+    <#
+        .SYNOPSIS
+        Gets the global Git configuration.
+
+        .DESCRIPTION
+        Gets the global Git configuration.
+
+        .EXAMPLE
+        Get-GitHubGitConfig
+
+        Gets the global Git configuration.
+    #>
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param()
+
+    $commandName = $MyInvocation.MyCommand.Name
+    Write-Verbose "[$commandName] - Start"
+
+    $gitExists = Get-Command -Name 'git' -ErrorAction SilentlyContinue
+    if (-not $gitExists) {
+        throw 'Git is not installed. Please install Git before running this command.'
+    }
+
+    git config --global --list | ForEach-Object {
+        (
+            [pscustomobject]@{
+                Name  = $_.Split('=')[0]
+                Value = $_.Split('=')[1]
+            }
+        )
+    }
+
+    Write-Verbose "[$commandName] - End"
+}
