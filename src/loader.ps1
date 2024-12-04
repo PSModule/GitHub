90f834e3333ddc6b262073f592c32a1ef94b304c
Marius Storhaug <marstor@hotmail.com>
2024-12-04 18:15:11 +0100

    during modulel loader: check module context first, and then update/add the specifics

--- src/loader.ps1
+++ src/loader.ps1
@@ -2,6 +2,19 @@
 Write-Verbose 'Initializing GitHub PowerShell module...'
 Write-Verbose "Path: $scriptFilePath"
 
+### This is the context for this module
+# Get current module context
+$context = (Get-Context -ID $script:Config.Name)
+if (-not $context) {
+    Write-Verbose 'No context found, creating a new context...'
+    Set-Context -ID $script:Config.Name -Context @{
+        RunEnv                        = $script:runEnv
+        GitHubAppClientID             = $script:Auth.GitHubApp.ClientID
+        OAuthAppClientID              = $script:Auth.OAuthApp.ClientID
+        AccessTokenGracePeriodInHours = $script:Auth.AccessTokenGracePeriodInHours
+    }
+}
+
 $script:runEnv = if ($env:GITHUB_ACTIONS -eq 'true') {
     'GHA'
 } elseif (-not [string]::IsNullOrEmpty($env:WEBSITE_PLATFORM_VERSION)) {
@@ -10,19 +23,11 @@ $script:runEnv = if ($env:GITHUB_ACTIONS -eq 'true') {
     'Local'
 }
 
+Set-GitHubConfig -Name 'RunEnv' -Value $script:runEnv
+
 if ($env:GITHUB_ACTIONS -eq 'true') {
     Write-Verbose 'Detected running on a GitHub Actions runner, preparing environment...'
     $env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
     Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
 }
 
-### This is the context for this module
-# Get current module context
-$context = (Get-Context -ID $script:Config.Name)
-if (-not $context) {
-    Write-Verbose 'No context found, creating a new context...'
-    Set-Context -ID $script:Config.Name -Context @{
-        Name   = 'GitHub'
-        RunEnv = $script:runEnv
-    }
-}
