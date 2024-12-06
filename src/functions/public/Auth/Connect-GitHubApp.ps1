4f917f3a4248ceac8ddffa30fc8bd19e00ffe5ed
Marius Storhaug <marstor@hotmail.com>
2024-12-06 17:18:13 +0100

    Optimize installation retrieval in Connect-GitHubApp with parallel processing and suppress verbose output

--- src/functions/public/Auth/Connect-GitHubApp.ps1
+++ src/functions/public/Auth/Connect-GitHubApp.ps1
@@ -96,7 +96,8 @@
             }
         }
 
-        foreach ($installation in $installations) {
+        $installations | ForEach-Object -ThrottleLimit [System.Environment]::ProcessorCount -Parallel {
+            $installation = $_
             $contextParams = $defaultContextData.Clone()
             $token = New-GitHubAppInstallationAccessToken -InstallationID $installation.id
             $contextParams += @{
@@ -140,23 +141,26 @@ Register-ArgumentCompleter -CommandName Connect-GitHubApp -ParameterName User -S
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    Get-GitHubAppInstallation | Where-Object { $_.target_type -eq 'User' -and $_.account.login -like "$wordToComplete*" } | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_.account.login, $_.account.login, 'ParameterValue', $_.account.login)
-    }
+    Get-GitHubAppInstallation -Verbose:$false | Where-Object { $_.target_type -eq 'User' -and $_.account.login -like "$wordToComplete*" } |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_.account.login, $_.account.login, 'ParameterValue', $_.account.login)
+        }
 }
 Register-ArgumentCompleter -CommandName Connect-GitHubApp -ParameterName Organization -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    Get-GitHubAppInstallation | Where-Object { $_.target_type -eq 'Organization' -and $_.account.login -like "$wordToComplete*" } | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_.account.login, $_.account.login, 'ParameterValue', $_.account.login)
-    }
+    Get-GitHubAppInstallation -Verbose:$false | Where-Object { $_.target_type -eq 'Organization' -and $_.account.login -like "$wordToComplete*" } |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_.account.login, $_.account.login, 'ParameterValue', $_.account.login)
+        }
 }
 Register-ArgumentCompleter -CommandName Connect-GitHubApp -ParameterName Enterprise -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    Get-GitHubAppInstallation | Where-Object { $_.target_type -eq 'Enterprise' -and $_.account.slug -like "$wordToComplete*" } | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_.account.slug, $_.account.slug, 'ParameterValue', $_.account.slug)
-    }
+    Get-GitHubAppInstallation -Verbose:$false | Where-Object { $_.target_type -eq 'Enterprise' -and $_.account.slug -like "$wordToComplete*" } |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_.account.slug, $_.account.slug, 'ParameterValue', $_.account.slug)
+        }
 }
