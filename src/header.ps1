f9a12f93263e3b27b08ccce7ab6c8d96ce5bf88e
Marius Storhaug <marstor@hotmail.com>
2024-12-07 23:00:48 +0100

    test

--- src/header.ps1
+++ src/header.ps1
@@ -1,4 +1,4 @@
-﻿using module modules/Context/999.0.0
+﻿using module 'modules/Context/999.0.0/Context.psd1'
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidLongLines', '', Justification = 'Contains long links.')]
 [CmdletBinding()]
