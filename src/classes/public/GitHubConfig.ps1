e613d311b972e2d3b38391e435d409a630e9c385
Marius Storhaug <marstor@hotmail.com>
2024-12-07 23:25:55 +0100

    Add properties to GitHubConfig class for configuration settings

--- src/classes/public/GitHubConfig.ps1
+++ src/classes/public/GitHubConfig.ps1
@@ -1,9 +1,20 @@
 ﻿class GitHubConfig : Context {
+    # The access token grace period in hours.
     [int] $AccessTokenGracePeriodInHours
+
+    # The default context.
     [string] $DefaultContext
+
+    # The default GitHub App client ID.
     [string] $DefaultGitHubAppClientID
+
+    # The default host name.
     [string] $DefaultHostName
+
+    # The default OAuth app client ID.
     [string] $DefaultOAuthAppClientID
+
+    # The type of run environment.
     [string] $RunEnv
 
     GitHubConfig() : Base([string]$ID) {}
