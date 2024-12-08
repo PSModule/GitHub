bd291d18d7f0f4d73145a85cc774f1e5f679ea07
Marius Storhaug <marstor@hotmail.com>
2024-12-08 10:11:51 +0100

    Refactor context retrieval to simplify syntax and improve readability

--- src/loader.ps1
+++ src/loader.ps1
@@ -4,10 +4,10 @@ Write-Verbose "Path: $scriptFilePath"
 
 ### This is the context for this module
 # Get current module context
-$context = [GitHubConfig](Get-Context -ID $script:Config.Name)
+$context = (Get-Context -ID $script:Config.Name)
 if (-not $context) {
     Write-Verbose 'No context found, creating a new context...'
-    $context = [GitHubConfig](Set-Context -ID $script:Config.Name -Context @{})
+    $context = (Set-Context -ID $script:Config.Name -Context @{})
 }
 
 if (-not $context.DefaultGitHubAppClientID) {
