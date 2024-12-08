8dd4ef56e10dc8755da343a4fff3d4578201e83f
Marius Storhaug <marstor@hotmail.com>
2024-12-08 16:29:02 +0100

    Refactor environment detection to use switch statement for GitHub Actions

--- src/loader.ps1
+++ src/loader.ps1
@@ -4,8 +4,10 @@ Write-Verbose "Path: $scriptFilePath"
 
 Initialize-GitHubConfig
 
-if ($env:GITHUB_ACTIONS -eq 'true') {
-    Write-Verbose 'Detected running on a GitHub Actions runner, preparing environment...'
-    $env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
-    Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
+switch ($script:GitHub.EnvironmentType) {
+    'GHA' {
+        Write-Verbose 'Detected running on a GitHub Actions runner, preparing environment...'
+        $env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
+        Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
+    }
 }
