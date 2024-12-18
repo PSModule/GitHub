7fb5f34951dec2cc15615b6dbaad1354104c13a0
Marius Storhaug <marstor@hotmail.com>
2024-12-18 18:42:16 +0100

    Refactor Get-GitHubTeam to use GraphQL API for fetching teams and add Get-GitHubRESTTeam for REST API support

--- src/functions/public/Teams/Get-GitHubTeam.ps1
+++ src/functions/public/Teams/Get-GitHubTeam.ps1
@@ -1,42 +1,31 @@
 ﻿function Get-GitHubTeam {
     <#
         .SYNOPSIS
-        List teams from an org or get a team by name
+        Get the teams for an organization using the GitHub GraphQL API.
 
         .DESCRIPTION
-        Lists all teams in an organization that are visible to the authenticated user or gets a team using the team's slug.
-        To create the slug, GitHub replaces special characters in the name string, changes all words to lowercase,
-        and replaces spaces with a - separator. For example, "My TEam Näme" would become my-team-name.
+        This command will get the teams for an organization using the GitHub GraphQL API.
 
         .EXAMPLE
-        Get-GitHubTeam -Organization 'github'
+        Get-GitHubTeam -Organization 'PSModule'
 
-        Gets all teams in the 'github' organization.
+        Gets the teams for the PSModule organization.
 
         .EXAMPLE
-        Get-GitHubTeamByName -Organization 'github' -Name 'my-team-name'
+        Get-GitHubTeam -Organization 'PSModule' -Context $Context
 
-        Gets the team with the slug 'my-team-name' in the 'github' organization.
-
-        .NOTES
-        [List teams](https://docs.github.com/rest/teams/teams#list-teams)
-        [Get team by name](https://docs.github.com/en/rest/teams/teams#get-a-team-by-name)
+        Gets the teams for the PSModule organization using the provided context.
     #>
-    [OutputType([pscustomobject])]
-    [CmdletBinding(DefaultParameterSetName = '__AllParameterSets')]
+    [CmdletBinding()]
     param(
-        # The organization name. The name is not case sensitive.
-        [Parameter(Mandatory)]
-        [Alias('Org')]
-        [string] $Organization,
-
-        # The slug of the team name.
-        [Parameter(
-            Mandatory,
-            ParameterSetName = 'GetByName'
-        )]
-        [Alias('Team', 'TeamName', 'slug', 'team_slug')]
+        # The name of the organization to get the teams for.
+        [Parameter()]
         [string] $Name,
+
+        # The owner of the organization to get the teams for.
+        # If not provided, the owner from the context will be used.
+        [Parameter()]
+        [string] $Organization,
 
         # The context to run the command in. Used to get the details for the API call.
         # Can be either a string or a GitHubContext object.
@@ -49,8 +38,7 @@
         Write-Debug "[$stackPath] - Start"
         $Context = Resolve-GitHubContext -Context $Context
         Assert-GitHubContext -Context $Context -AuthType IAT, PAT, UAT
-
-        if ([string]::IsNullOrEmpty($Organization)) {
+        if ([string]::IsNullOrEmpty($Owner)) {
             $Organization = $Context.Owner
         }
         Write-Debug "Organization: [$Organization]"
@@ -58,17 +46,86 @@
 
     process {
         try {
-            $params = @{
-                Organization = $Organization
-                Context      = $Context
+            $teamQuery = $null -ne $Name ? 'slug: `$teamSlug' : 'first: 100, after: `$after'
+            $query = @"
+query(`$org: String!, `$after: String) {
+  organization(login: `$org) {
+    teams($teamQuery) {
+      nodes {
+        id
+        name
+        slug
+        combinedSlug
+        databaseId
+        description
+        notificationSetting
+        privacy
+        parentTeam {
+          name
+          slug
+        }
+        organization {
+          login
+        }
+        childTeams(first: 100) {
+          nodes {
+            name
+          }
+        }
+        createdAt
+        updatedAt
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+"@
+
+            # Variables hash that will be sent with the query
+            $variables = @{
+                org      = $Organization
+                teamSlug = $Name
             }
-            switch ($PSCmdlet.ParameterSetName) {
-                'GetByName' {
-                    Get-GitHubTeamByName @params -Name $Name
+
+            # Prepare to store results and handle pagination
+            $hasNextPage = $true
+            $after = $null
+
+            while ($hasNextPage) {
+                # Update the cursor for pagination
+                $variables['after'] = $after
+
+                # Send the request to the GitHub GraphQL API
+                $response = Invoke-GitHubGraphQLQuery -Query $query -Variables $variables
+
+                # Extract team data
+                $teams = $response.data.organization.teams
+
+                # Accumulate the teams in results
+                $teams.nodes | ForEach-Object {
+                    [PSCustomObject]@{
+                        Name          = $_.name # PSModule Admins
+                        Slug          = $_.slug # psmodule-admins
+                        NodeID        = $_.id # T_kwDOCIVCh84AgoiD
+                        CombinedSlug  = $_.combinedSlug # PSModule/psmodule-admins
+                        DatabaseId    = $_.databaseId # 8554627
+                        Description   = $_.description #
+                        Notifications = $_.notificationSetting -eq 'NOTIFICATIONS_ENABLED' ? $true : $false
+                        Privacy       = $_.privacy # VISIBLE
+                        ParentTeam    = $_.parentTeam.slug
+                        Organization  = $_.organization.login
+                        ChildTeams    = $_.childTeams.nodes.name
+                        CreatedAt     = $_.createdAt # 9/9/2023 11:15:12 AM
+                        UpdatedAt     = $_.updatedAt # 3/10/2024 4:42:05 PM
+                    }
                 }
-                '__AllParameterSets' {
-                    Get-GitHubTeamListByOrg @params
-                }
+
+                # Check if there's another page to fetch
+                $hasNextPage = $teams.pageInfo.hasNextPage
+                $after = $teams.pageInfo.endCursor
             }
         } catch {
             throw $_
