ff9ac5074daa34326548eb187cb7f31a072de841
Marius Storhaug <marstor@hotmail.com>
2024-12-22 21:38:21 +0100

    🩹 [Patch]: Add function to uninstall a GitHub App from an enterprise organization (#235)
    ## Description
    
    This pull request introduces a new PowerShell function to uninstall a
    GitHub App from an enterprise organization. The function includes
    parameters for the enterprise, organization, and installation ID, and
    makes an API call to remove the specified installation.
    
    New functionality:
    
    *
    [`src/functions/public/Enterprise/Uninstall-GitHubAppOnEnterpriseOrganization.ps1`](diffhunk://#diff-28129cb6b97ff80748123a1dd7472ae8b7dbfb34e8b4156bd0ac6f36d72a8baaR1-R68):
    Added the `Uninstall-GitHubAppOnEnterpriseOrganization` function to
    uninstall a GitHub App from an organization.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/functions/public/Enterprise/Uninstall-GitHubAppOnEnterpriseOrganization.ps1
@@ -0,0 +1,68 @@
+﻿function Uninstall-GitHubAppOnEnterpriseOrganization {
+    <#
+        .SYNOPSIS
+        Uninstall a GitHub App from an organization.
+
+        .DESCRIPTION
+        Uninstall a GitHub App from an organization.
+
+        The authenticated GitHub App must be installed on the enterprise and be granted the Enterprise/organization_installations (write) permission.
+
+        .EXAMPLE
+        Uninstall-GitHubAppOnEnterpriseOrganization -Enterprise 'github' -Organization 'octokit' -InstallationID '123456'
+
+        Uninstall the GitHub App with the installation ID `123456` from the organization `octokit` in the enterprise `github`.
+    #>
+    #SkipTest:FunctionTest:Will add a test for this function in a future PR
+    [CmdletBinding()]
+    param(
+        # The enterprise slug or ID.
+        [Parameter()]
+        [string] $Enterprise,
+
+        # The organization name. The name is not case sensitive.
+        [Parameter(Mandatory)]
+        [string] $Organization,
+
+        # The client ID of the GitHub App to install.
+        [Parameter(Mandatory)]
+        [Alias('installation_id')]
+        [string] $InstallationID,
+
+        # The context to run the command in. Used to get the details for the API call.
+        # Can be either a string or a GitHubContext object.
+        [Parameter()]
+        [object] $Context = (Get-GitHubContext)
+    )
+
+    begin {
+        $stackPath = Get-PSCallStackPath
+        Write-Debug "[$stackPath] - Start"
+        $Context = Resolve-GitHubContext -Context $Context
+        Assert-GitHubContext -Context $Context -AuthType IAT, PAT, UAT
+        if ([string]::IsNullOrEmpty($Enterprise)) {
+            $Enterprise = $Context.Enterprise
+        }
+        Write-Debug "Enterprise: [$Enterprise]"
+    }
+
+    process {
+        try {
+            $inputObject = @{
+                Context     = $Context
+                APIEndpoint = "/enterprises/$Enterprise/apps/organizations/$Organization/installations/$InstallationID}"
+                Method      = 'Delete'
+            }
+
+            Invoke-GitHubAPI @inputObject | ForEach-Object {
+                Write-Output $_.Response
+            }
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Debug "[$stackPath] - End"
+    }
+}
