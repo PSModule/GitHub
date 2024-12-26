c7ade68ea2accd5ab47c9994bb1331fca9fd1027
Marius Storhaug <marstor@hotmail.com>
2024-12-26 15:29:35 +0100

    🩹 [Refactor]: Rename Set-GitHubUser function to Update-GitHubUser and update references

--- src/functions/public/Users/Update-GitHubUser.ps1
+++ src/functions/public/Users/Update-GitHubUser.ps1
@@ -1,4 +1,4 @@
-﻿filter Set-GitHubUser {
+﻿filter Update-GitHubUser {
     <#
         .SYNOPSIS
         Update the authenticated user
@@ -9,17 +9,17 @@
         displayed on your public profile or via the API.
 
         .EXAMPLE
-        Set-GitHubUser -Name 'octocat'
+        Update-GitHubUser -Name 'octocat'
 
         Update the authenticated user's name to 'octocat'
 
         .EXAMPLE
-        Set-GitHubUser -Location 'San Francisco'
+        Update-GitHubUser -Location 'San Francisco'
 
         Update the authenticated user's location to 'San Francisco'
 
         .EXAMPLE
-        Set-GitHubUser -Hireable $true -Bio 'I love programming'
+        Update-GitHubUser -Hireable $true -Bio 'I love programming'
 
         Update the authenticated user's hiring availability to 'true' and their biography to 'I love programming'
 
@@ -27,7 +27,6 @@
         [Update the authenticated user](https://docs.github.com/rest/users/users#update-the-authenticated-user)
     #>
     [OutputType([void])]
-    [Alias('Update-GitHubUser')]
     [CmdletBinding(SupportsShouldProcess)]
     param(
         # The new name of the user.
