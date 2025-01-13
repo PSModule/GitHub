6c58b38fc3129e84a754382326431632cc4da93b
Marius Storhaug <marstor@hotmail.com>
2025-01-13 13:05:27 +0100

    Enhance Get-GitHubGitConfig to support configurable scope and mask sensitive data

--- src/functions/public/Git/Get-GitHubGitConfig.ps1
+++ src/functions/public/Git/Get-GitHubGitConfig.ps1
@@ -13,11 +13,16 @@
     #>
     [OutputType([pscustomobject])]
     [CmdletBinding()]
-    param()
+    param(
+        [Parameter()]
+        [ValidateSet('local', 'global', 'system')]
+        [string] $Scope = 'local'
+    )
 
     begin {
         $stackPath = Get-PSCallStackPath
         Write-Debug "[$stackPath] - Start"
+        $result = @{}
     }
 
     process {
@@ -26,7 +31,7 @@
             $gitExists = Get-Command -Name 'git' -ErrorAction SilentlyContinue
             Write-Debug "GITEXISTS: $gitExists"
             if (-not $gitExists) {
-                Write-Verbose "Git is not installed. Cannot get git configuration."
+                Write-Verbose 'Git is not installed. Cannot get git configuration.'
                 return
             }
 
@@ -40,13 +45,18 @@
                 return
             }
 
-            git config --local --list | ForEach-Object {
-                (
-                    [pscustomobject]@{
-                        Name  = $_.Split('=')[0]
-                        Value = $_.Split('=')[1]
+            git config --$Scope --list | ConvertFrom-StringData | ForEach-Object {
+                $_.GetEnumerator() | ForEach-Object {
+                    $name = $_.Key
+                    $value = $_.Value
+                    if ($value -match '(?i)AUTHORIZATION:\s*(?<scheme>[^\s]+)\s+(?<token>.*)') {
+                        $secret = $matches['token']
+                        Add-GitHubMask -Value $secret
                     }
-                )
+                    $result += @{
+                        $name = $value
+                    }
+                }
             }
         } catch {
             throw $_
