987ec4ccfb3b8d1175cde174c7ac976e7fdecc14
Marius Storahug <marstor@hotmail.com>
2025-01-16 20:55:15 +0100

    Add ssh replacer to git config

--- src/functions/public/Git/Set-GitHubGitConfig.ps1
+++ src/functions/public/Git/Set-GitHubGitConfig.ps1
@@ -55,14 +55,17 @@
             $token = $Context.Token | ConvertFrom-SecureString -AsPlainText
             $hostName = $Context.HostName
             $installationName = $Context.InstallationName
+            $sshUser = $Context.Enterprise ?? 'git'
 
             if ($PSCmdlet.ShouldProcess("$username on $installationName", 'Set Git configuration')) {
                 $git = 'git'
                 @(
                     @('config', '--local', 'user.name', "$username"),
                     @('config', '--local', 'user.email', "$id+$username@users.noreply.github.com"),
-                    @('config', '--local', "url.https://oauth2:$token@$hostName/$installationName.insteadOf",
+                    @('config', '--local', "url.`"https://oauth2:$token@$hostName/$installationName`".insteadOf",
                         "https://$hostName/$installationName")
+                    @('config', '--local', "url.`"https://oauth2:$token@$hostName/$installationName`".insteadOf",
+                        "ssh://$sshUser@$hostName`:$installationName")
                 ) | ForEach-Object {
                     Write-Verbose "$git $($_ -join ' ')"
                     & $git $_
