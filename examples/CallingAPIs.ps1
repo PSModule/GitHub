a4fe1e4751cb59d2a44d0e5274a46bd9151250d2
Marius Storhaug <marstor@hotmail.com>
2025-01-17 22:52:19 +0100

    🩹 [Patch]: Update API calls in CallingAPIs.ps1 to use ApiBaseUri and fix method usage

--- examples/CallingAPIs.ps1
+++ examples/CallingAPIs.ps1
@@ -9,13 +9,15 @@ Invoke-GitHubAPI -ApiEndpoint /user
 
 # Most complex example - output is the entire web response
 $context = Get-GitHubContext
-Invoke-WebRequest -Uri "https://api.$($context.HostName)/user" -Token ($context.Token) -Authentication Bearer
+Invoke-RestMethod -Uri "$($context.ApiBaseUri)user" -Token ($context.Token) -Authentication Bearer
+Invoke-WebRequest -Uri "$($context.ApiBaseUri)user" -Token ($context.Token) -Authentication Bearer
 #endregion
 
 
 #region As an app: get the authenticated app
 $ClientID = ''
-$PrivateKey = ''
+$PrivateKey = @'
+'@
 Connect-GitHub -ClientID $ClientID -PrivateKey $PrivateKey
 
 # Simple example - output is the object
@@ -27,21 +29,23 @@ Invoke-GitHubAPI -ApiEndpoint /app
 # Most complex example - output is the entire web response
 $context = Get-GitHubContext
 $jwt = Get-GitHubAppJSONWebToken -ClientId $context.ClientID -PrivateKey $context.Token
-Invoke-WebRequest -Uri "https://api.$($context.HostName)/user" -Token ($jwt.token) -Authentication Bearer
+Invoke-RestMethod -Uri "$($context.ApiBaseUri)/app" -Token ($jwt.token) -Authentication Bearer
+Invoke-WebRequest -Uri "$($context.ApiBaseUri)/app" -Token ($jwt.token) -Authentication Bearer
+
 #endregion
 
 
 #region As an app installation: get zen
-Connect-GitHubApp -Installation 'PSModule'
+Connect-GitHubApp -Organization 'PSModule'
 
 # Simple example - output is the object
-Get-GitHuben
+Get-GitHubZen
 
 # More complex example - output is parts of the web response
 Invoke-GitHubAPI -ApiEndpoint /zen
 
 # Most complex example - output is the entire web response
 $context = Get-GitHubContext
-Invoke-WebRequest -Uri "https://api.$($context.HostName)/zen" -Token ($context.Token) -Authentication Bearer
-Invoke-RestMethod -Uri "https://api.$($context.HostName)/octocat" -Token ($context.Token) -Authentication Bearer
+Invoke-RestMethod -Uri "$($context.ApiBaseUri)/octocat" -Token ($context.Token) -Authentication Bearer
+Invoke-WebRequest -Uri "$($context.ApiBaseUri)/zen" -Token ($context.Token) -Authentication Bearer
 #endregion
