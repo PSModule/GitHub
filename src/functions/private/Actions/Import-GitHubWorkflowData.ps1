499b5835d0495827e25598fb57cde5590489fce0
Marius Storhaug <marstor@hotmail.com>
2025-01-17 23:24:15 +0100

    🩹 [Patch]: Add Import-GitHubWorkflowData and Get-GitHubWorkflowData functions; remove Get-GitHubEvent function

---
+++ src/functions/private/Actions/Import-GitHubWorkflowData.ps1
@@ -0,0 +1,66 @@
+﻿function Import-GitHubWorkflowData {
+    <#
+        .SYNOPSIS
+        Import data from the workflow and the event that triggered the workflow
+
+        .DESCRIPTION
+        Import data from the workflow and the event that triggered the workflow
+
+        .EXAMPLE
+        Import-GitHubWorkflowData
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSUseShouldProcessForStateChangingFunctions', '',
+        Justification = 'Just setting a value in a variable.'
+    )]
+    [OutputType([pscustomobject])]
+    [CmdletBinding()]
+    param()
+
+    begin {
+        $stackPath = Get-PSCallStackPath
+        Write-Debug "[$stackPath] - Start"
+    }
+
+    process {
+        try {
+            $gitHubEventJson = Get-Content -Path $env:GITHUB_EVENT_PATH
+            $gitHubEvent = $gitHubEventJson | ConvertFrom-Json
+
+            $eventAction = $gitHubEvent.action
+            $eventSender = $gitHubEvent.sender | Select-Object -Property login, type, id, node_id, html_url
+            $eventEnterprise = $gitHubEvent.enterprise | Select-Object -Property name, slug, id, node_id, html_url
+            $eventOrganization = $gitHubEvent.organization | Select-Object -Property login, id, node_id
+            $eventOwner = $gitHubEvent.repository.owner | Select-Object -Property login, type, id, node_id, html_url
+            $eventRepository = $gitHubEvent.repository | Select-Object -Property name, full_name, html_url, id, node_id, default_branch
+
+            $gitHubEvent = $gitHubEvent | Select-Object -ExcludeProperty action, sender, enterprise, organization, repository
+            $gitHubEvent | Add-Member -MemberType NoteProperty -Name Name -Value $env:GITHUB_EVENT_NAME -Force
+            if ($eventAction) {
+                $gitHubEvent | Add-Member -MemberType NoteProperty -Name Action -Value $eventAction -Force
+            }
+            if ($eventSender) {
+                $gitHubEvent | Add-Member -MemberType NoteProperty -Name Sender -Value $eventSender -Force
+            }
+            if ($eventEnterprise) {
+                $gitHubEvent | Add-Member -MemberType NoteProperty -Name Enterprise -Value $eventEnterprise -Force
+            }
+            if ($eventOrganization) {
+                $gitHubEvent | Add-Member -MemberType NoteProperty -Name Organization -Value $eventOrganization -Force
+            }
+            if ($eventOwner) {
+                $gitHubEvent | Add-Member -MemberType NoteProperty -Name Owner -Value $eventOwner -Force
+            }
+            if ($eventRepository) {
+                $gitHubEvent | Add-Member -MemberType NoteProperty -Name Repository -Value $eventRepository -Force
+            }
+            $script:GitHub.Event = $gitHubEvent
+        } catch {
+            throw $_
+        }
+    }
+
+    end {
+        Write-Debug "[$stackPath] - End"
+    }
+}
---
+++ src/functions/public/Actions/Get-GitHubWorkflowData.ps1
@@ -0,0 +1,28 @@
+﻿function Get-GitHubWorkflowData {
+    <#
+        .SYNOPSIS
+        Get data from the workflow and the event that triggered the workflow
+
+        .DESCRIPTION
+        Get data from the workflow and the event that triggered the workflow
+
+        .EXAMPLE
+        Get-GitHubWorkflowData
+    #>
+    [CmdletBinding()]
+    param()
+
+    begin {
+        $stackPath = Get-PSCallStackPath
+        Write-Debug "[$stackPath] - Start"
+    }
+
+    process {
+        $script:GitHub.Event
+    }
+
+    end {
+        Write-Debug "[$stackPath] - End"
+    }
+}
+
