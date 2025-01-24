9764809604fc60e1dcaaefcd8063cd0fc971296c
Marius Storhaug <marstor@hotmail.com>
2025-01-24 16:04:22 +0100

    Add repository selection parameters to Install-GitHubApp function

--- src/functions/public/Apps/GitHub Apps/Install-GitHubApp.ps1
+++ src/functions/public/Apps/GitHub Apps/Install-GitHubApp.ps1
@@ -45,6 +45,17 @@
         [Parameter(Mandatory)]
         [string] $ClientID,
 
+        # The repository selection for the GitHub App. Can be one of:
+        # - all - all repositories that the authenticated GitHub App installation can access.
+        # - selected - select specific repositories.
+        [Parameter()]
+        [ValidateSet('all', 'selected')]
+        [string] $RepositorySelection = 'selected',
+
+        # The names of the repositories to which the installation will be granted access.
+        [Parameter()]
+        [string[]] $Repositories = @(),
+
         # The context to run the command in. Used to get the details for the API call.
         # Can be either a string or a GitHubContext object.
         [Parameter()]
