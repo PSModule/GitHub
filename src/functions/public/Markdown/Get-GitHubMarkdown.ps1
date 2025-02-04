33d75da455691a2a9f4241ee29332a51199664e4
Marius Storhaug <marstor@hotmail.com>
2025-02-04 01:34:12 +0100

    🩹 [Refactor]: Update comment formatting for clarity in Get-GitHubMarkdown function

--- src/functions/public/Markdown/Get-GitHubMarkdown.ps1
+++ src/functions/public/Markdown/Get-GitHubMarkdown.ps1
@@ -31,8 +31,8 @@
         [ValidateSet('markdown', 'gfm')]
         [string] $Mode = 'markdown',
 
-        # The repository context to use when creating references in `gfm` mode. For example, setting `context` to `octo-org/octo-Repository` will change the
-        # text `#42` into an HTML link to issue 42 in the `octo-org/octo-Repository` repository.
+        # The repository context to use when creating references in `gfm` mode. For example, setting `context` to `octo-org/octo-Repository` will
+        # change the text `#42` into an HTML link to issue 42 in the `octo-org/octo-Repository` repository.
         [Parameter()]
         [string] $RepoContext,
 
