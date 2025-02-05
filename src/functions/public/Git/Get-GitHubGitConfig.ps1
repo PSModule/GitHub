c82fcdc30c278daf812458a9abc4b56f098a40f1
Marius Storhaug <marstor@hotmail.com>
2025-02-05 10:10:17 +0100

    🩹 [Patch]: Refactor Get-GitHubGitConfig function for improved readability and maintainability

--- src/functions/public/Git/Get-GitHubGitConfig.ps1
+++ src/functions/public/Git/Get-GitHubGitConfig.ps1
@@ -25,43 +25,43 @@
     }
 
     process {
-            $gitExists = Get-Command -Name 'git' -ErrorAction SilentlyContinue
-            Write-Debug "GITEXISTS: $gitExists"
-            if (-not $gitExists) {
-                Write-Verbose 'Git is not installed. Cannot get git configuration.'
-                return
-            }
+        $gitExists = Get-Command -Name 'git' -ErrorAction SilentlyContinue
+        Write-Debug "GITEXISTS: $gitExists"
+        if (-not $gitExists) {
+            Write-Verbose 'Git is not installed. Cannot get git configuration.'
+            return
+        }
 
-            $cmdresult = git rev-parse --is-inside-work-tree 2>&1
-            Write-Debug "LASTEXITCODE: $LASTEXITCODE"
-            Write-Debug "CMDRESULT:    $cmdresult"
-            if ($LASTEXITCODE -ne 0) {
-                Write-Verbose 'Not a git repository. Cannot get git configuration.'
-                $Global:LASTEXITCODE = 0
-                Write-Debug "Resetting LASTEXITCODE: $LASTEXITCODE"
-                return
-            }
+        $cmdresult = git rev-parse --is-inside-work-tree 2>&1
+        Write-Debug "LASTEXITCODE: $LASTEXITCODE"
+        Write-Debug "CMDRESULT:    $cmdresult"
+        if ($LASTEXITCODE -ne 0) {
+            Write-Verbose 'Not a git repository. Cannot get git configuration.'
+            $Global:LASTEXITCODE = 0
+            Write-Debug "Resetting LASTEXITCODE: $LASTEXITCODE"
+            return
+        }
 
-            $config = @()
-            $configList = git config --$Scope --list 2>&1
-            if ($LASTEXITCODE -ne 0) {
-                Write-Verbose "Failed to get git configuration for [$Scope]."
-                $global:LASTEXITCODE = 0
-                Write-Debug "Resetting LASTEXITCODE: $LASTEXITCODE"
-                return $config
-            }
+        $config = @()
+        $configList = git config --$Scope --list 2>&1
+        if ($LASTEXITCODE -ne 0) {
+            Write-Verbose "Failed to get git configuration for [$Scope]."
+            $global:LASTEXITCODE = 0
+            Write-Debug "Resetting LASTEXITCODE: $LASTEXITCODE"
+            return $config
+        }
 
-            $configList = $configList | Sort-Object
-            $configList | ForEach-Object {
-                Write-Debug "CONFIG: $_"
-                $name, $value = $_ -split '=', 2
-                $name = ('' + $name).Trim()
-                $value = ('' + $value).Trim()
-                $config += @{
-                    $name = $value
-                }
+        $configList = $configList | Sort-Object
+        $configList | ForEach-Object {
+            Write-Debug "CONFIG: $_"
+            $name, $value = $_ -split '=', 2
+            $name = ('' + $name).Trim()
+            $value = ('' + $value).Trim()
+            $config += @{
+                $name = $value
             }
-            $config
+        }
+        $config
 
     }
 
