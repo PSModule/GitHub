76df72abe69cbba182b37ce9ca58a39ee258572c
Marius Storhaug <marstor@hotmail.com>
2025-02-17 17:38:31 +0100

    🚀 [Feature]: Add GITHUB_HOST_NAME environment variable for GitHub Actions

--- src/loader.ps1
+++ src/loader.ps1
@@ -7,6 +7,8 @@ switch ($script:GitHub.EnvironmentType) {
         Write-Verbose 'Detected running on a GitHub Actions runner, preparing environment...'
         $env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
         Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
+        $env:GITHUB_HOST_NAME = ($env:GITHUB_SERVER_URL ?? 'github.com') -replace '^https?://'
+        Set-GitHubEnv -Name 'GITHUB_HOST_NAME' -Value $env:GITHUB_HOST_NAME
         Import-GitHubEventData
         Import-GitHubRunnerData
     }
