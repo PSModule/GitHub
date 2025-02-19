a13c5c8acef0846a60c6c747d74fa41cbf1e4308
Marius Storhaug <marstor@hotmail.com>
2025-02-20 00:52:27 +0100

    🩹 [Refactor]: Update required version of Hashtable module to 1.1.1 in Invoke-GitHubAPI.ps1

--- src/functions/public/API/Invoke-GitHubAPI.ps1
+++ src/functions/public/API/Invoke-GitHubAPI.ps1
@@ -1,5 +1,5 @@
 ﻿#Requires -Modules @{ ModuleName = 'Uri'; RequiredVersion = '1.1.0' }
-#Requires -Modules @{ ModuleName = 'Hashtable'; RequiredVersion = '1.0.0' }
+#Requires -Modules @{ ModuleName = 'Hashtable'; RequiredVersion = '1.1.1' }
 
 filter Invoke-GitHubAPI {
     <#
