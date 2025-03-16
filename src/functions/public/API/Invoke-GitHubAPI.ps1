c9158914689d8a0bd50609fbd3f2237999f0f0bf
Marius Storhaug <marstor@hotmail.com>
2025-03-16 20:55:07 +0100

    🩹 [Refactor]: Improve error message formatting in Invoke-GitHubAPI function for better readability

--- src/functions/public/API/Invoke-GitHubAPI.ps1
+++ src/functions/public/API/Invoke-GitHubAPI.ps1
@@ -284,13 +284,13 @@ filter Invoke-GitHubAPI {
             $APICall.Headers = $APICall.Headers | ConvertTo-Json
             $APICall.Method = $APICall.Method.ToString()
 
-            $errorResult = @"
-----------------------------------
-Error details:
-$($errorResult | Format-Table -AutoSize -HideTableHeaders | Out-String)
-----------------------------------
-"@
-            Write-Error $errorResult
+            $errorResult = [System.Collections.ArrayList]::new()
+            $null = $errorResult.Add('----------------------------------')
+            $null = $errorResult.Add('Error details:')
+            $null = $($errorResult | Format-Table -AutoSize -HideTableHeaders | Out-String -Stream | ForEach-Object { $errorResult.Add($_) })
+            $null = $errorResult.Add('----------------------------------')
+
+            Write-Error ($errorResult -join [System.Environment]::NewLine)
             throw $failure.Exception.Message
         }
     }
