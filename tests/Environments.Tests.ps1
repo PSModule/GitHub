54ca1f2cd5420019f797a15cf138eccd64c954a9
Marius Storhaug <marstor@hotmail.com>
2025-03-16 21:17:15 +0100

    🩹 [Refactor]: Update repository naming in tests to include unique GUID suffix for better isolation

--- tests/Environments.Tests.ps1
+++ tests/Environments.Tests.ps1
@@ -12,7 +12,7 @@
 param()
 
 BeforeAll {
-    $repo = 'EnvironmentTest'
+    $repoSuffix = 'EnvironmentTest'
     $environmentName = 'production'
 }
 
@@ -20,6 +20,8 @@ Describe 'As a user - Fine-grained PAT token - user account access (USER_FG_PAT)
     BeforeAll {
         Connect-GitHubAccount -Token $env:TEST_USER_USER_FG_PAT
         $owner = 'psmodule-user'
+        $guid = [guid]::NewGuid().ToString()
+        $repo = "$repoSuffix-$guid"
         New-GitHubRepository -Name $repo -AllowSquashMerge
     }
     AfterAll {
@@ -70,6 +72,8 @@ Describe 'As a user - Fine-grained PAT token - organization account access (ORG_
     BeforeAll {
         Connect-GitHubAccount -Token $env:TEST_USER_ORG_FG_PAT
         $owner = 'psmodule-test-org2'
+        $guid = [guid]::NewGuid().ToString()
+        $repo = "$repoSuffix-$guid"
         New-GitHubRepository -Owner $owner -Name $repo -AllowSquashMerge
     }
     AfterAll {
@@ -124,6 +128,8 @@ Describe 'As a GitHub App - Enterprise (APP_ENT)' {
     BeforeAll {
         Connect-GitHubAccount -ClientID $env:TEST_APP_ENT_CLIENT_ID -PrivateKey $env:TEST_APP_ENT_PRIVATE_KEY
         $owner = 'psmodule-test-org3'
+        $guid = [guid]::NewGuid().ToString()
+        $repo = "$repoSuffix-$guid"
         Connect-GitHubApp -Organization $owner -Default
         New-GitHubRepository -Owner $owner -Name $repo -AllowSquashMerge
     }
@@ -175,6 +181,8 @@ Describe 'As a GitHub App - Organization (APP_ORG)' {
     BeforeAll {
         Connect-GitHubAccount -ClientID $env:TEST_APP_ORG_CLIENT_ID -PrivateKey $env:TEST_APP_ORG_PRIVATE_KEY
         $owner = 'psmodule-test-org'
+        $guid = [guid]::NewGuid().ToString()
+        $repo = "$repoSuffix-$guid"
         Connect-GitHubApp -Organization $owner -Default
         New-GitHubRepository -Owner $owner -Name $repo -AllowSquashMerge
     }
