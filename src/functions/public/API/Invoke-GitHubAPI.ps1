e48d72d3699a03acfa5a02395001ed67b75e140e
Marius Storhaug <marstor@hotmail.com>
2025-03-19 11:46:24 +0100

    🩹 [Patch]: Add 'User-Agent' header to Invoke-GitHubAPI function

--- src/functions/public/API/Invoke-GitHubAPI.ps1
+++ src/functions/public/API/Invoke-GitHubAPI.ps1
@@ -122,6 +122,7 @@ filter Invoke-GitHubAPI {
         $headers = @{
             Accept                 = $Accept
             'X-GitHub-Api-Version' = $ApiVersion
+            'User-Agent'           = 'PSModule.GitHub'
         }
         $headers | Remove-HashtableEntry -NullOrEmptyValues
 
