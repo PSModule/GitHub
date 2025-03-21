a5644a618b2c59ba69fe05e2334f62142d554368
Marius Storhaug <marstor@hotmail.com>
2025-03-21 10:25:20 +0100

    🩹 [Refactor]: Improve test structure for Get-GitHubUser and email management by consolidating related assertions and enhancing readability

--- tests/User.Tests.ps1
+++ tests/User.Tests.ps1
@@ -47,37 +47,38 @@ Context 'User' {
             { Get-GitHubUser -Username 'Octocat' } | Should -Not -Throw
         }
 
-        if ($OwnerType -eq 'user')
-        It 'Get-GitHubUser - Gets the authenticated user' {
-            { Get-GitHubUser } | Should -Not -Throw
-        }
-        It 'Update-GitHubUser - Can set configuration on a user' {
-            $guid = (New-Guid).Guid
-            $user = Get-GitHubUser
-            { Update-GitHubUser -Name 'Octocat' } | Should -Not -Throw
-            { Update-GitHubUser -Blog 'https://psmodule.io' } | Should -Not -Throw
-            { Update-GitHubUser -TwitterUsername 'PSModule' } | Should -Not -Throw
-            { Update-GitHubUser -Company 'PSModule' } | Should -Not -Throw
-            { Update-GitHubUser -Location 'USA' } | Should -Not -Throw
-            { Update-GitHubUser -Bio 'I love programming' } | Should -Not -Throw
-            $tmpUser = Get-GitHubUser
-            $tmpUser.name | Should -Be 'Octocat'
-            $tmpUser.blog | Should -Be 'https://psmodule.io'
-            $tmpUser.twitter_username | Should -Be 'PSModule'
-            $tmpUser.company | Should -Be 'PSModule'
-            $tmpUser.location | Should -Be 'USA'
-            $tmpUser.bio | Should -Be 'I love programming'
-        }
-        Context 'Email' {
-            It 'Get-GitHubUserEmail - Gets all email addresses for the authenticated user' {
-                { Get-GitHubUserEmail } | Should -Not -Throw
+        if ($OwnerType -eq 'user') {
+            It 'Get-GitHubUser - Gets the authenticated user' {
+                { Get-GitHubUser } | Should -Not -Throw
             }
-            It 'Add/Remove-GitHubUserEmail - Adds and removes an email to the authenticated user' {
-                $email = (New-Guid).Guid + '@psmodule.io'
-                { Add-GitHubUserEmail -Email $email } | Should -Not -Throw
+            It 'Update-GitHubUser - Can set configuration on a user' {
+                $guid = (New-Guid).Guid
+                $user = Get-GitHubUser
+                { Update-GitHubUser -Name 'Octocat' } | Should -Not -Throw
+                { Update-GitHubUser -Blog 'https://psmodule.io' } | Should -Not -Throw
+                { Update-GitHubUser -TwitterUsername 'PSModule' } | Should -Not -Throw
+                { Update-GitHubUser -Company 'PSModule' } | Should -Not -Throw
+                { Update-GitHubUser -Location 'USA' } | Should -Not -Throw
+                { Update-GitHubUser -Bio 'I love programming' } | Should -Not -Throw
+                $tmpUser = Get-GitHubUser
+                $tmpUser.name | Should -Be 'Octocat'
+                $tmpUser.blog | Should -Be 'https://psmodule.io'
+                $tmpUser.twitter_username | Should -Be 'PSModule'
+                $tmpUser.company | Should -Be 'PSModule'
+                $tmpUser.location | Should -Be 'USA'
+                $tmpUser.bio | Should -Be 'I love programming'
+            }
+            Context 'Email' {
+                It 'Get-GitHubUserEmail - Gets all email addresses for the authenticated user' {
+                    { Get-GitHubUserEmail } | Should -Not -Throw
+                }
+                It 'Add/Remove-GitHubUserEmail - Adds and removes an email to the authenticated user' {
+                    $email = (New-Guid).Guid + '@psmodule.io'
+                    { Add-GitHubUserEmail -Email $email } | Should -Not -Throw
                 (Get-GitHubUserEmail).email | Should -Contain $email
-                { Remove-GitHubUserEmail -Email $email } | Should -Not -Throw
+                    { Remove-GitHubUserEmail -Email $email } | Should -Not -Throw
                 (Get-GitHubUserEmail).email | Should -Not -Contain $email
+                }
             }
         }
     }
