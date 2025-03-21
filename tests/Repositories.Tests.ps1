6ee2eb81325b932f3cec09cf56596cc75bb62bf7
Marius Storhaug <marstor@hotmail.com>
2025-03-21 01:01:16 +0100

    🩹 [Refactor]: Streamline repository access tests by consolidating user account scenarios and enhancing readability

--- tests/Repositories.Tests.ps1
+++ tests/Repositories.Tests.ps1
@@ -15,96 +15,77 @@
 [CmdletBinding()]
 param()
 
-Describe 'As a user - Fine-grained PAT token - user account access (USER_FG_PAT)' {
-    BeforeAll {
-        Connect-GitHubAccount -Token $env:TEST_USER_USER_FG_PAT
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-    Context 'Repository' {
+Describe 'Template' {
+    $authCases = . "$PSScriptRoot/Data/AuthCases.ps1"
+
+    Context 'As <Type> using <Case> on <Target>' -ForEach $authCases {
+        BeforeAll {
+            $context = Connect-GitHubAccount @connectParams -PassThru -Silent
+            LogGroup 'Context' {
+                Write-Host ($context | Format-List | Out-String)
+            }
+        }
+
+        # Tests for APP goes here
+        if ($AuthType -eq 'APP') {
+            It 'Connect-GitHubApp - Connects as a GitHub App to <Owner>' {
+                $context = Connect-GitHubApp @connectAppParams -PassThru -Default -Silent
+                LogGroup 'Context' {
+                    Write-Host ($context | Format-List | Out-String)
+                }
+            }
+        }
+
+        # Tests for runners goes here
+        if ($Type -eq 'GitHub Actions') {}
+
+        # Tests for IAT UAT and PAT goes here
+
         It "Get-GitHubRepository - Gets the authenticated user's repositories (USER_FG_PAT)" {
-            { Get-GitHubRepository } | Should -Not -Throw
+            $repo = Get-GitHubRepository
+            LogGroup 'Repository' {
+                Write-Host ($repo | Format-Table | Out-String)
+            }
+            $repo | Should -Not -BeNullOrEmpty
         }
         It "Get-GitHubRepository - Gets the authenticated user's public repositories (USER_FG_PAT)" {
-            { Get-GitHubRepository -Type 'public' } | Should -Not -Throw
+            $repo = Get-GitHubRepository -Type 'public'
+            LogGroup 'Repository' {
+                Write-Host ($repo | Format-Table | Out-String)
+            }
+            $repo | Should -Not -BeNullOrEmpty
         }
         It 'Get-GitHubRepository - Gets the public repos where the authenticated user is owner (USER_FG_PAT)' {
-            { Get-GitHubRepository -Visibility 'public' -Affiliation 'owner' } | Should -Not -Throw
+            $repo = Get-GitHubRepository -Visibility 'public' -Affiliation 'owner'
+            LogGroup 'Repository' {
+                Write-Host ($repo | Format-Table | Out-String)
+            }
+            $repo | Should -Not -BeNullOrEmpty
         }
         It 'Get-GitHubRepository - Gets a specific repository (USER_FG_PAT)' {
-            { Get-GitHubRepository -Owner 'PSModule' -Name 'GitHub' } | Should -Not -Throw
+            $repo = Get-GitHubRepository -Owner 'PSModule' -Name 'GitHub'
+            LogGroup 'Repository' {
+                Write-Host ($repo | Format-Table | Out-String)
+            }
+            $repo | Should -Not -BeNullOrEmpty
         }
         It 'Get-GitHubRepository - Gets all repositories from a organization (USER_FG_PAT)' {
-            { Get-GitHubRepository -Owner 'PSModule' } | Should -Not -Throw
+            $repo = Get-GitHubRepository -Owner 'PSModule'
+            LogGroup 'Repository' {
+                Write-Host ($repo | Format-Table | Out-String)
+            }
+            $repo | Should -Not -BeNullOrEmpty
         }
         It 'Get-GitHubRepository - Gets all repositories from a user (USER_FG_PAT)' {
-            { Get-GitHubRepository -Username 'MariusStorhaug' } | Should -Not -Throw
+            $repo = Get-GitHubRepository -Username 'MariusStorhaug'
+            LogGroup 'Repository' {
+                Write-Host ($repo | Format-Table | Out-String)
+            }
+            $repo | Should -Not -BeNullOrEmpty
         }
     }
 }
 
-Describe 'As a user - Fine-grained PAT token - organization account access (ORG_FG_PAT)' {
-    BeforeAll {
-        Connect-GitHubAccount -Token $env:TEST_USER_ORG_FG_PAT
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-    Context 'Repository' {
-        It "Get-GitHubRepository - Gets the authenticated user's repositories (ORG_FG_PAT)" {
-            { Get-GitHubRepository } | Should -Not -Throw
-        }
-        It "Get-GitHubRepository - Gets the authenticated user's public repositories (ORG_FG_PAT)" {
-            { Get-GitHubRepository -Type 'public' } | Should -Not -Throw
-        }
-        It 'Get-GitHubRepository - Gets the public repos where the authenticated user is owner (ORG_FG_PAT)' {
-            { Get-GitHubRepository -Visibility 'public' -Affiliation 'owner' } | Should -Not -Throw
-        }
-        It 'Get-GitHubRepository - Gets a specific repository (ORG_FG_PAT)' {
-            { Get-GitHubRepository -Owner 'PSModule' -Name 'GitHub' } | Should -Not -Throw
-        }
-        It 'Get-GitHubRepository - Gets all repositories from a organization (ORG_FG_PAT)' {
-            { Get-GitHubRepository -Owner 'PSModule' } | Should -Not -Throw
-        }
-        It 'Get-GitHubRepository - Gets all repositories from a user (ORG_FG_PAT)' {
-            { Get-GitHubRepository -Username 'MariusStorhaug' } | Should -Not -Throw
-        }
-    }
-}
-
-Describe 'As a user - Classic PAT token (PAT)' {
-    BeforeAll {
-        Connect-GitHubAccount -Token $env:TEST_USER_PAT
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-}
-
-Describe 'As GitHub Actions (GHA)' {
-    BeforeAll {
-        Connect-GitHubAccount
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-}
-
-Describe 'As a GitHub App - Enterprise (APP_ENT)' {
-    BeforeAll {
-        Connect-GitHubAccount -ClientID $env:TEST_APP_ENT_CLIENT_ID -PrivateKey $env:TEST_APP_ENT_PRIVATE_KEY
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-}
-
-Describe 'As a GitHub App - Organization (APP_ORG)' {
-    BeforeAll {
-        Connect-GitHubAccount -ClientID $env:TEST_APP_ORG_CLIENT_ID -PrivateKey $env:TEST_APP_ORG_PRIVATE_KEY
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
+AfterAll {
+    Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount -Silent
 }
--- tests/User.Tests.ps1
+++ tests/User.Tests.ps1
@@ -15,21 +15,40 @@
 [CmdletBinding()]
 param()
 
-Describe 'As a user - Fine-grained PAT token - user account access (USER_FG_PAT)' {
-    BeforeAll {
-        Connect-GitHubAccount -Token $env:TEST_USER_USER_FG_PAT
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-    Context 'User' {
-        It 'Get-GitHubUser - Gets the authenticated user (USER_FG_PAT)' {
-            { Get-GitHubUser } | Should -Not -Throw
+Context 'User' {
+    $authCases = . "$PSScriptRoot/Data/AuthCases.ps1"
+
+    Context 'As <Type> using <Case> on <Target>' -ForEach $authCases {
+        BeforeAll {
+            $context = Connect-GitHubAccount @connectParams -PassThru -Silent
+            LogGroup 'Context' {
+                Write-Host ($context | Format-List | Out-String)
+            }
         }
-        It 'Get-GitHubUser - Get the specified user (USER_FG_PAT)' {
+
+        # Tests for APP goes here
+        if ($AuthType -eq 'APP') {
+            It 'Connect-GitHubApp - Connects as a GitHub App to <Owner>' {
+                $context = Connect-GitHubApp @connectAppParams -PassThru -Default -Silent
+                LogGroup 'Context' {
+                    Write-Host ($context | Format-List | Out-String)
+                }
+            }
+        }
+
+        # Tests for runners goes here
+        if ($Type -eq 'GitHub Actions') {}
+
+        # Tests for IAT UAT and PAT goes here
+        It 'Get-GitHubUser - Get the specified user' {
             { Get-GitHubUser -Username 'Octocat' } | Should -Not -Throw
         }
-        It 'Update-GitHubUser - Can set configuration on a user (USER_FG_PAT)' {
+
+        if ($OwnerType -eq 'user')
+        It 'Get-GitHubUser - Gets the authenticated user' {
+            { Get-GitHubUser } | Should -Not -Throw
+        }
+        It 'Update-GitHubUser - Can set configuration on a user' {
             $guid = (New-Guid).Guid
             $user = Get-GitHubUser
             { Update-GitHubUser -Name 'Octocat' } | Should -Not -Throw
@@ -47,10 +66,10 @@ Describe 'As a user - Fine-grained PAT token - user account access (USER_FG_PAT)
             $tmpUser.bio | Should -Be 'I love programming'
         }
         Context 'Email' {
-            It 'Get-GitHubUserEmail - Gets all email addresses for the authenticated user (USER_FG_PAT)' {
+            It 'Get-GitHubUserEmail - Gets all email addresses for the authenticated user' {
                 { Get-GitHubUserEmail } | Should -Not -Throw
             }
-            It 'Add/Remove-GitHubUserEmail - Adds and removes an email to the authenticated user (USER_FG_PAT)' {
+            It 'Add/Remove-GitHubUserEmail - Adds and removes an email to the authenticated user' {
                 $email = (New-Guid).Guid + '@psmodule.io'
                 { Add-GitHubUserEmail -Email $email } | Should -Not -Throw
                 (Get-GitHubUserEmail).email | Should -Contain $email
@@ -61,97 +80,6 @@ Describe 'As a user - Fine-grained PAT token - user account access (USER_FG_PAT)
     }
 }
 
-Describe 'As a user - Fine-grained PAT token - organization account access (ORG_FG_PAT)' {
-    BeforeAll {
-        Connect-GitHubAccount -Token $env:TEST_USER_ORG_FG_PAT
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-    Context 'User' {
-        It 'Get-GitHubUser - Gets the authenticated user (ORG_FG_PAT)' {
-            { Get-GitHubUser } | Should -Not -Throw
-        }
-        It 'Get-GitHubUser - Get the specified user (ORG_FG_PAT)' {
-            { Get-GitHubUser -Username 'Octocat' } | Should -Not -Throw
-        }
-    }
-}
-
-Describe 'As a user - Classic PAT token (PAT)' {
-    BeforeAll {
-        Connect-GitHubAccount -Token $env:TEST_USER_PAT
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-    Context 'User' {
-        It 'Get-GitHubUser - Gets the authenticated user (PAT)' {
-            { Get-GitHubUser } | Should -Not -Throw
-        }
-        It 'Get-GitHubUser - Get the specified user (PAT)' {
-            { Get-GitHubUser -Username 'Octocat' } | Should -Not -Throw
-        }
-        It 'Update-GitHubUser - Can set configuration on a user (PAT)' {
-            $guid = (New-Guid).Guid
-            $user = Get-GitHubUser
-            { Update-GitHubUser -Name 'Octocat' } | Should -Not -Throw
-            { Update-GitHubUser -Blog 'https://psmodule.io' } | Should -Not -Throw
-            { Update-GitHubUser -TwitterUsername 'PSModule' } | Should -Not -Throw
-            { Update-GitHubUser -Company 'PSModule' } | Should -Not -Throw
-            { Update-GitHubUser -Location 'USA' } | Should -Not -Throw
-            { Update-GitHubUser -Bio 'I love programming' } | Should -Not -Throw
-            $tmpUser = Get-GitHubUser
-            $tmpUser.name | Should -Be 'Octocat'
-            $tmpUser.blog | Should -Be 'https://psmodule.io'
-            $tmpUser.twitter_username | Should -Be 'PSModule'
-            $tmpUser.company | Should -Be 'PSModule'
-            $tmpUser.location | Should -Be 'USA'
-            $tmpUser.bio | Should -Be 'I love programming'
-        }
-        Context 'Email' {
-            It 'Get-GitHubUserEmail - Gets all email addresses for the authenticated user (PAT)' {
-                { Get-GitHubUserEmail } | Should -Not -Throw
-            }
-            It 'Add/Remove-GitHubUserEmail - Adds and removes an email to the authenticated user (PAT)' {
-                $email = (New-Guid).Guid + '@psmodule.io'
-                { Add-GitHubUserEmail -Email $email } | Should -Not -Throw
-                (Get-GitHubUserEmail).email | Should -Contain $email
-                { Remove-GitHubUserEmail -Email $email } | Should -Not -Throw
-                (Get-GitHubUserEmail).email | Should -Not -Contain $email
-            }
-        }
-    }
-}
-
-Describe 'As GitHub Actions (GHA)' {
-    BeforeAll {
-        Connect-GitHubAccount
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-    Context 'User' {
-        It 'Get-GitHubUser - Get the specified user (GHA)' {
-            { Get-GitHubUser -Username 'Octocat' } | Should -Not -Throw
-        }
-    }
-}
-
-Describe 'As a GitHub App - Enterprise (APP_ENT)' {
-    BeforeAll {
-        Connect-GitHubAccount -ClientID $env:TEST_APP_ENT_CLIENT_ID -PrivateKey $env:TEST_APP_ENT_PRIVATE_KEY
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
-}
-
-Describe 'As a GitHub App - Organization (APP_ORG)' {
-    BeforeAll {
-        Connect-GitHubAccount -ClientID $env:TEST_APP_ORG_CLIENT_ID -PrivateKey $env:TEST_APP_ORG_PRIVATE_KEY
-    }
-    AfterAll {
-        Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount
-    }
+AfterAll {
+    Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount -Silent
 }
