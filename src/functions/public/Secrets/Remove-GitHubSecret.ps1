d727b844b52cb3a2906e49548c4e5c16c8bbb3a2
Marius Storhaug <marstor@hotmail.com>
2025-04-06 14:47:52 +0200

    🩹 [Enhancement]: Add InputObject parameter to Remove-GitHubSecret function for improved flexibility in handling GitHub secrets

--- src/functions/public/Secrets/Remove-GitHubSecret.ps1
+++ src/functions/public/Secrets/Remove-GitHubSecret.ps1
@@ -61,7 +61,10 @@
         # The context to run the command in. Used to get the details for the API call.
         # Can be either a string or a GitHubContext object.
         [Parameter()]
-        [object] $Context = (Get-GitHubContext)
+        [object] $Context = (Get-GitHubContext),
+
+        [Parameter(Mandatory, ParameterSetName = 'ArrayInput', ValueFromPipeline)]
+        [GitHubSecret[]] $InputObject
     )
 
     begin {
