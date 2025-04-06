a026077d2e44f3a1ead5a39cbcaf9bb511ec95b9
Marius Storhaug <marstor@hotmail.com>
2025-04-06 13:34:18 +0200

    🩹 [Fix]: Update Get-GitHubSecret call in organization context to include Debug and Verbose parameters for better logging

--- tests/Secrets.Tests.ps1
+++ tests/Secrets.Tests.ps1
@@ -78,7 +78,7 @@ Describe 'Secrets' {
                     Remove-GitHubRepository -Owner $owner -Name $repoName -Confirm:$false
                 }
                 'organization' {
-                    # Get-GitHubSecret -Owner $owner -Debug -Verbose | Remove-GitHubSecret -Debug -Verbose
+                    Get-GitHubSecret -Owner $owner -Debug -Verbose | Remove-GitHubSecret -Debug -Verbose
                     Remove-GitHubRepository -Owner $owner -Name $repoName -Confirm:$false
                     Remove-GitHubRepository -Owner $owner -Name "$repoName-2" -Confirm:$false
                     Remove-GitHubRepository -Owner $owner -Name "$repoName-3" -Confirm:$false
