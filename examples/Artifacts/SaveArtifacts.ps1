453171539df7354e6c2eb06b5d8d1bce56503a8a
Marius Storhaug <marstor@hotmail.com>
2025-04-12 01:58:26 +0200

    🩹 [Patch]: Add example commands for creating and managing a GitHub repository and release in SaveArtifacts.ps1

--- examples/Artifacts/SaveArtifacts.ps1
+++ examples/Artifacts/SaveArtifacts.ps1
@@ -1,3 +1,17 @@
 ﻿$modulesPath = $env:PSModulePath -Split [IO.Path]::PathSeparator | Select-Object -First 1
 Get-GitHubArtifact -Owner PSModule -Repository GitHub -Name module |
     Save-GitHubArtifact -Path $modulesPath -Extract -Force
+
+
+
+New-GitHubRepository -Name mytest -AllowSquashMerge -AddReadme -License mit -Gitignore VisualStudio
+Get-GitHubRepository -Username MariusStorhaug -Name mytest | Remove-GitHubRepository
+New-GitHubRelease -Owner MariusStorhaug -Repository mytest -Name mytest -Tag v1.0 -Body 'Initial release'
+
+
+Get-GitHubOrganization -Name PSModule | Get-GitHubRepository -Name GitHub | Get-GitHubWorkflow -Name Process-PSModule
+Get-GitHubOrganization | Get-GitHubRepository -Name GitHub | Get-GitHubRelease
+Get-GitHubUser | Get-GitHubRepository -Name mytest
+
+
+Get-GitHubRelease -Owner PSModule -Repository GitHub
