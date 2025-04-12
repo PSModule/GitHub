cc3ca5d328841498ca40e03e9bbcd98640a0aeb0
Marius Storhaug <marstor@hotmail.com>
2025-04-12 12:55:16 +0200

    🩹 [Patch]: Update SaveArtifacts.ps1 to add new GitHub API calls for branch creation and retrieval; modify GitHubRelease.Format.ps1xml to include 'Url' column and remove 'Author' column for improved clarity.

--- examples/Artifacts/SaveArtifacts.ps1
+++ examples/Artifacts/SaveArtifacts.ps1
@@ -16,4 +16,15 @@ Get-GitHubUser | Get-GitHubRepository | Get-GitHubRelease
 Get-GitHubRelease -Owner PSModule -Repository GitHub
 
 Get-GitHubUser | Get-GitHubRepository -Name mytest | New-GitHubRelease -Tag 'v1.6.3' -Latest -GenerateReleaseNotes -Notes 'Release notes' -Name 'test'
-Get-GitHubUser | Get-GitHubRepository -Name mytest | Get-GitHubRelease -All | Select *
+Get-GitHubUser | Get-GitHubRepository -Name mytest | Get-GitHubRelease -All
+
+
+Invoke-GitHubAPI -ApiEndpoint '/repos/PSModule/Github/git/matching-refs/' | Select-Object -ExpandProperty Response | Select * | Format-Table
+
+Invoke-GitHubAPI -ApiEndpoint '/repos/MariusStorhaug/mytest/branches' | Select-Object -ExpandProperty Response
+
+
+Invoke-GitHubAPI -Method POST -ApiEndpoint '/repos/MariusStorhaug/mytest/git/refs' -Body @{
+    ref = 'refs/heads/test'
+    sha = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0'
+}
