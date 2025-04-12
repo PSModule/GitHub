62e96d848639cdaf3154323c7e77e9af7dbdebd2
Marius Storhaug <marstor@hotmail.com>
2025-04-12 12:02:07 +0200

    🩹 [Patch]: Update SaveArtifacts.ps1 to remove unnecessary blank lines; modify GitHubRelease.Format.ps1xml to add 'Owner' and 'Repository' columns and adjust existing column labels for clarity.

--- examples/Artifacts/SaveArtifacts.ps1
+++ examples/Artifacts/SaveArtifacts.ps1
@@ -7,6 +7,9 @@ Get-GitHubRepository -Username MariusStorhaug -Name mytest | Remove-GitHubReposi
 New-GitHubRelease -Owner MariusStorhaug -Repository mytest -Name 'mytest' -Tag 'v1.0' -Body 'Initial release' -Debug
 
 Get-GitHubOrganization -Name PSModule | Get-GitHubRepository -Name GitHub | Get-GitHubWorkflow -Name Process-PSModule | Get-gitHubWorkflowRun
+
+
+
 Get-GitHubOrganization | Get-GitHubRepository -Name GitHub | Get-GitHubRelease
 Get-GitHubUser | Get-GitHubRepository -Name mytest | Get-GitHubRelease
 
@@ -14,4 +17,3 @@ Get-GitHubRelease -Owner PSModule -Repository GitHub
 
 Get-GitHubUser | Get-GitHubRepository -Name mytest | New-GitHubRelease -Tag 'v1.6.3' -Latest -GenerateReleaseNotes -Notes 'Release notes' -Name 'test'
 Get-GitHubUser | Get-GitHubRepository -Name mytest | Get-GitHubRelease -All | Select *
-
