6a1b53b9dc868156cbef5796b7df108038cc102e
Marius Storhaug <marstor@hotmail.com>
2025-04-17 09:51:40 +0200

    🚀 [Feature]: Add workflows script and enhance GitHubWorkflowRun class properties

---
+++ examples/Actions/Workflows.ps1
@@ -0,0 +1,8 @@
+﻿# Gets all workflows in the PSModule organization and starts the 'Nightly Run' workflow
+'PSModule' | Get-GitHubOrganization | Get-GitHubRepository | Get-GitHubWorkflow -Name 'Nightly Run'
+'PSModule' | Get-GitHubOrganization | Get-GitHubRepository | Get-GitHubWorkflow -Name 'Nightly Run' | Start-GitHubWorkflow
+
+
+'PSModule' | Get-GitHubOrganization | Get-GitHubRepository | Get-GitHubWorkflow -Name 'Nightly Run' | ForEach-Object {
+    $_ | Get-GitHubWorkflowRun | Select-Object -First 1
+}
