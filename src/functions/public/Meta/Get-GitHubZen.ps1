fde2d278c8ba72a0280799f7b26377fa55db8fe0
Marius Storhaug <marstor@hotmail.com>
2025-04-17 10:59:09 +0200

    Add initial test scripts for GitHub integration
    - Created TEMPLATE.ps1 for structuring Pester tests with GitHub authentication.
    - Added Users.Tests.ps1 to test user-related GitHub API functionalities, including user retrieval and updates.
    - Introduced Variables.Tests.ps1 to validate GitHub variable management across different scopes (organization, repository, environment).
    - Implemented logging and context management for better test output and organization.

--- src/functions/public/Meta/Get-GitHubZen.ps1
+++ src/functions/public/Meta/Get-GitHubZen.ps1
@@ -16,6 +16,10 @@
     #>
     [CmdletBinding()]
     param(
+        # If specified, makes an anonymous request to the GitHub API without authentication.
+        [Parameter()]
+        [switch] $Anonymous,
+
         # The context to run the command in. Used to get the details for the API call.
         # Can be either a string or a GitHubContext object.
         [Parameter()]
@@ -25,14 +29,17 @@
     begin {
         $stackPath = Get-PSCallStackPath
         Write-Debug "[$stackPath] - Start"
-        $Context = Resolve-GitHubContext -Context $Context
-        Assert-GitHubContext -Context $Context -AuthType IAT, PAT, UAT
+        if (-not $Anonymous) {
+            $Context = Resolve-GitHubContext -Context $Context
+            Assert-GitHubContext -Context $Context -AuthType IAT, PAT, UAT
+        }
     }
 
     process {
         $inputObject = @{
             Method      = 'GET'
             APIEndpoint = '/zen'
+            Anonymous   = $Anonymous
             Context     = $Context
         }
 
