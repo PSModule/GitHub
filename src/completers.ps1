c21162fdf3214472705fe56a5008bb2680547901
Marius Storhaug <marstor@hotmail.com>
2025-04-19 22:08:23 +0200

    🩹 [Refactor]: Enhance argument completer to check for Anonymous parameter and include it in context options

--- src/completers.ps1
+++ src/completers.ps1
@@ -1,8 +1,24 @@
 ﻿Register-ArgumentCompleter -ParameterName Context -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
+    $null = $commandName, $parameterName, $wordToComplete, $fakeBoundParameter
 
-    Get-GitHubContext -ListAvailable -Verbose:$false | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+    $contexts = @()
+
+    # Check if the command has an Anonymous parameter
+    $hasAnonymousParameter = $false
+    if ($commandAst -and $commandAst.CommandElements -and $commandAst.CommandElements.Count -gt 1) {
+        $command = Get-Command -Name $commandAst.CommandElements[0].Value -ErrorAction SilentlyContinue
+        if ($command) {
+            $hasAnonymousParameter = $command.Parameters.ContainsKey('Anonymous')
+        }
+    }
+    if ($hasAnonymousParameter) {
+        $contexts += 'Anonymous'
+    }
+
+    $contexts += (Get-GitHubContext -ListAvailable -Verbose:$false).Name
+
+    $contexts | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, "ListIItem: $($_)", 'ParameterValue', "Tooltip: $($_)")
     }
 }
