9d95f149172a4b4531174cc9539d3bc9ad667b3e
Marius Storhaug <marstor@hotmail.com>
2025-04-28 12:52:03 +0200

    Refactor argument completers: remove unused Owner completer and add Get-GitHubOrganization completer

--- src/completers.ps1
+++ src/completers.ps1
@@ -17,26 +17,3 @@
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
-
-Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport) -ParameterName Owner -ScriptBlock {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
-
-    Get-GitHubOwner -Verbose:$false -Context $fakeBoundParameter['Context'] | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    } | Where-Object { $_.CompletionText -like "$wordToComplete*" }
-}
-
-# Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport) -ParameterName Repository -ScriptBlock {
-#     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-#     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
-
-#     $repos = if ($fakeBoundParameter.ContainsKey('Owner')) {
-#         Get-GitHubRepository -Verbose:$false -Owner $fakeBoundParameter['Owner']
-#     } else {
-#         Get-GitHubRepository -Verbose:$false
-#     }
-#     $repos | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
-#         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-#     }
-# }
---
+++ src/functions/public/Organization/completers.ps1
@@ -0,0 +1,9 @@
+﻿Register-ArgumentCompleter -CommandName Get-GitHubOrganization -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
+
+    Get-GitHubOrganization -Verbose:$false -Context $fakeBoundParameter.Context |
+        Where-Object { $_.CompletionText -like "$wordToComplete*" } | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
