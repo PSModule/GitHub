c928bcb1ebf81d8aca5b8e3596b5d129b1e3dcff
Marius Storhaug <marstor@hotmail.com>
2025-05-15 22:17:44 +0200

    🚀 [Refactor]: Remove unnecessary quotes from command names in argument completers for consistency.

--- src/functions/public/Repositories/completers.ps1
+++ src/functions/public/Repositories/completers.ps1
@@ -1,4 +1,4 @@
-﻿Register-ArgumentCompleter -CommandName 'New-GitHubRepository' -ParameterName Gitignore -ScriptBlock {
+﻿Register-ArgumentCompleter -CommandName New-GitHubRepository -ParameterName Gitignore -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
     Get-GitHubGitignore | Select-Object -ExpandProperty name | Where-Object { $_ -like "*$wordToComplete*" } | ForEach-Object {
@@ -6,7 +6,7 @@
     }
 }
 
-Register-ArgumentCompleter -CommandName 'New-GitHubRepository' -ParameterName License -ScriptBlock {
+Register-ArgumentCompleter -CommandName New-GitHubRepository -ParameterName License -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
     Get-GitHubLicense | Select-Object -ExpandProperty name | Where-Object { $_ -like "*$wordToComplete*" } | ForEach-Object {
@@ -26,6 +26,19 @@ Register-ArgumentCompleter -CommandName Get-GitHubRepository -ParameterName Prop
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
     [GitHubRepository].GetProperties().Name | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport) -ParameterName Repository -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
+    $owner = if ($fakeBoundParameters.ContainsKey('Owner')) {
+        $fakeBoundParameters.Owner
+    } elseif ($fakeBoundParameters.ContainsKey('Organization')) {
+        $fakeBoundParameters.Organization
+    }
+    (Get-GitHubRepository -Owner $owner -Property Name -Verbose:$false).Name | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
