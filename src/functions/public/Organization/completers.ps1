0c985376aede0eabdd8311a8a8a7ceaa7623a6ba
Marius Storhaug <marstor@hotmail.com>
2025-05-16 20:50:38 +0200

    🚀 [Add]: Implement argument completers for GitHubOrganization to enhance command-line usability.

---
+++ src/functions/public/Organization/completers.ps1
@@ -0,0 +1,15 @@
+﻿Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport | Where-Object { $_ -like '*GitHubOrganization' }) -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
+    (Get-GitHubOrganization).Name | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport) -ParameterName Owner -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
+    (Get-GitHubOrganization).Name | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
