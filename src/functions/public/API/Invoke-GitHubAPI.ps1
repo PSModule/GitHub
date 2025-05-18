98a0b374d939a434e25eb629c0923bc94e9a9970
Marius Storhaug <marstor@hotmail.com>
2025-05-18 11:23:50 +0200

    🚀 [Fix]: Remove conditional assignment for per_page parameter in Invoke-GitHubAPI function

--- src/functions/public/API/Invoke-GitHubAPI.ps1
+++ src/functions/public/API/Invoke-GitHubAPI.ps1
@@ -177,10 +177,7 @@ filter Invoke-GitHubAPI {
             if (-not $Body) {
                 $Body = @{}
             }
-            $calledDirectly = (Get-PSCallStack).Command -like '*-GitHub*'
-            if ($PSBoundParameters.ContainsKey('PerPage') -or $calledDirectly) {
-                $Body['per_page'] = Resolve-GitHubContextSetting -Name 'PerPage' -Value $PerPage -Context $Context
-            }
+            $Body['per_page'] = Resolve-GitHubContextSetting -Name 'PerPage' -Value $PerPage -Context $Context
             $APICall.Uri = New-Uri -BaseUri $Uri -Query $Body -AsString
         } elseif (($Method -eq 'POST') -and -not [string]::IsNullOrEmpty($UploadFilePath)) {
             $APICall.Uri = New-Uri -BaseUri $Uri -Query $Body -AsString
