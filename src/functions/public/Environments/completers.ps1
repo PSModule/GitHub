e68395993078fe0380f44aae24d803b60e88e7d9
Marius Storhaug <marstor@hotmail.com>
2025-05-19 20:50:02 +0200

    ✨ [Feature]: Add argument completers for GitHubEnvironment commands and module functions

---
+++ src/functions/public/Environments/completers.ps1
@@ -0,0 +1,32 @@
+﻿# Complete the Name parameter for all *GitHubEnvironment commands
+Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport |
+        Where-Object { $_ -like '*GitHubEnvironment' }) -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
+
+    $params = @{
+        Owner      = $fakeBoundParameter.Owner
+        Repository = $fakeBoundParameter.Repository
+        Context    = $fakeBoundParameter.Context
+    }
+    $params | Remove-HashtableEntry -NullOrEmptyValues
+    Get-GitHubEnvironment @params | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+    }
+}
+
+# Complete the Environment parameter for all functions in the module
+Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport) -ParameterName Environment -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
+
+    $params = @{
+        Owner      = $fakeBoundParameter.Owner
+        Repository = $fakeBoundParameter.Repository
+        Context    = $fakeBoundParameter.Context
+    }
+    $params | Remove-HashtableEntry -NullOrEmptyValues
+    Get-GitHubEnvironment @params | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+    }
+}
