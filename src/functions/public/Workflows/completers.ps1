3729109bbf40b292758f9bf55bb95ff4b20f2183
Marius Storhaug <marstor@hotmail.com>
2025-05-19 22:18:10 +0200

    ✨ [Feature]: Implement argument completers for GitHub teams, variables, and workflows

---
+++ src/functions/public/Workflows/completers.ps1
@@ -0,0 +1,28 @@
+﻿Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport |
+        Where-Object { $_ -like '*GitHubWorkflow' }) -ParameterName Name -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
+
+    $params = @{
+        Owner      = $fakeBoundParameter.Owner
+        Repository = $fakeBoundParameter.Repository
+    }
+    $params | Remove-HashtableEntry -NullOrEmptyValues
+    Get-GitHubWorkflow @params | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
+    }
+}
+Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport |
+        Where-Object { $_ -like '*GitHubWorkflow' }) -ParameterName ID -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
+
+    $params = @{
+        Owner      = $fakeBoundParameter.Owner
+        Repository = $fakeBoundParameter.Repository
+    }
+    $params | Remove-HashtableEntry -NullOrEmptyValues
+    Get-GitHubWorkflow @params | Where-Object { $_.ID -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.ID, "$($_.Name) ($($_.ID))", 'ParameterValue', "$($_.Name) ($($_.ID))"  )
+    }
+}
