fddd20b1421180b7e0bfe3efb2c8aa7225e9da1a
Marius Storhaug <marstor@hotmail.com>
2025-05-23 16:29:00 +0200

    🩹 [Patch]: Comment out existing tests for Set-GitHubRepository to prevent execution during testing

--- tests/Repositories.Tests.ps1
+++ tests/Repositories.Tests.ps1
@@ -226,47 +226,47 @@ Describe 'Repositories' {
             }
             $repos.Count | Should -BeGreaterThan 0
         }
-        It 'Set-GitHubRepository - Updates an existing repository' -Skip:($OwnerType -eq 'repository') {
-            $description = 'Updated description'
-            LogGroup 'Repository - Set update' {
-                # Get current repo config
-                switch ($OwnerType) {
-                    'user' {
-                        $repoBefore = Get-GitHubRepository -Name $repoName
-                        $repo = Set-GitHubRepository -Name $repoName -Description $description
-                    }
-                    'organization' {
-                        $repoBefore = Get-GitHubRepository -Owner $owner -Name $repoName
-                        $repo = Set-GitHubRepository -Owner $owner -Name $repoName -Description $description
-                    }
-                }
-                Write-Host ($repo | Format-List | Out-String)
-                $changes = Compare-PSCustomObject -Left $repoBefore -Right $repo -OnlyChanged
-                Write-Host ('Changed properties: ' + ($changes | Format-Table | Out-String))
-            }
-            $repo | Should -Not -BeNullOrEmpty
-            $repo.Description | Should -Be $description
-            $changedProps = $changes.Property
-            $changedProps | Should -Contain 'UpdatedAt'
-            $changedProps | Should -Contain 'Description'
-            $changedProps.Count | Should -Be 2
-        }
-        It 'Set-GitHubRepository - Creates a new repository when missing' -Skip:($OwnerType -eq 'repository') {
-            $newRepoName = "$repoName-new"
-            LogGroup 'Repository - Set create' {
-                switch ($OwnerType) {
-                    'user' {
-                        $repo = Set-GitHubRepository -Name $newRepoName -Description 'Set create'
-                    }
-                    'organization' {
-                        $repo = Set-GitHubRepository -Owner $owner -Name $newRepoName -Description 'Set create'
-                    }
-                }
-                Write-Host ($repo | Format-List | Out-String)
-            }
-            $repo | Should -Not -BeNullOrEmpty
-            $repo.Name | Should -Be $newRepoName
-        }
+        # It 'Set-GitHubRepository - Updates an existing repository' -Skip:($OwnerType -eq 'repository') {
+        #     $description = 'Updated description'
+        #     LogGroup 'Repository - Set update' {
+        #         # Get current repo config
+        #         switch ($OwnerType) {
+        #             'user' {
+        #                 $repoBefore = Get-GitHubRepository -Name $repoName
+        #                 $repo = Set-GitHubRepository -Name $repoName -Description $description
+        #             }
+        #             'organization' {
+        #                 $repoBefore = Get-GitHubRepository -Owner $owner -Name $repoName
+        #                 $repo = Set-GitHubRepository -Owner $owner -Name $repoName -Description $description
+        #             }
+        #         }
+        #         Write-Host ($repo | Format-List | Out-String)
+        #         $changes = Compare-PSCustomObject -Left $repoBefore -Right $repo -OnlyChanged
+        #         Write-Host ('Changed properties: ' + ($changes | Format-Table | Out-String))
+        #     }
+        #     $repo | Should -Not -BeNullOrEmpty
+        #     $repo.Description | Should -Be $description
+        #     $changedProps = $changes.Property
+        #     $changedProps | Should -Contain 'UpdatedAt'
+        #     $changedProps | Should -Contain 'Description'
+        #     $changedProps.Count | Should -Be 2
+        # }
+        # It 'Set-GitHubRepository - Creates a new repository when missing' -Skip:($OwnerType -eq 'repository') {
+        #     $newRepoName = "$repoName-new"
+        #     LogGroup 'Repository - Set create' {
+        #         switch ($OwnerType) {
+        #             'user' {
+        #                 $repo = Set-GitHubRepository -Name $newRepoName -Description 'Set create'
+        #             }
+        #             'organization' {
+        #                 $repo = Set-GitHubRepository -Owner $owner -Name $newRepoName -Description 'Set create'
+        #             }
+        #         }
+        #         Write-Host ($repo | Format-List | Out-String)
+        #     }
+        #     $repo | Should -Not -BeNullOrEmpty
+        #     $repo.Name | Should -Be $newRepoName
+        # }
         It 'Update-GitHubRepository - Renames a repository' -Skip:($OwnerType -eq 'repository') {
             LogGroup 'Repository - Renamed' {
                 $newName = "$repoName-newname"
