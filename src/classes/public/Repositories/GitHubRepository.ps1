de1176725472dd2d1d22f64ef8f3728c401da1c4
Marius Storhaug <marstor@hotmail.com>
2025-05-25 15:31:07 +0200

    Update DefaultBranch query to include additional fields for improved repository data retrieval

--- src/classes/public/Repositories/GitHubRepository.ps1
+++ src/classes/public/Repositories/GitHubRepository.ps1
@@ -226,7 +226,7 @@
         Stargazers               = 'stargazers { totalCount }'
         Watchers                 = 'watchers { totalCount }'
         Forks                    = 'forks { totalCount }'
-        DefaultBranch            = 'defaultBranchRef { name }'
+        DefaultBranch            = 'defaultBranchRef { id name prefix target { id oid abbreviatedOid } }'
         Permissions              = 'viewerPermission'
         AllowSquashMerge         = 'squashMergeAllowed'
         AllowMergeCommit         = 'mergeCommitAllowed'
