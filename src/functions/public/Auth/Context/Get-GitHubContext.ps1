40b42fef53dde443122abb1eb52eefea627ffa0b
Marius Storhaug <marstor@hotmail.com>
2025-06-08 10:14:40 +0200

    🩹 [Patch]: Update verbose messages in Get-GitHubContext for clarity and consistency

--- src/functions/public/Auth/Context/Get-GitHubContext.ps1
+++ src/functions/public/Auth/Context/Get-GitHubContext.ps1
@@ -51,7 +51,7 @@
             }
             'ListAvailableContexts' {
                 Write-Verbose "ListAvailable: [$ListAvailable]"
-                $ID = "*"
+                $ID = '*'
             }
             default {
                 Write-Verbose 'Getting default context.'
@@ -61,9 +61,9 @@
                 }
             }
         }
-        Write-Verbose "Getting the default context: [$ID]"
+        Write-Verbose "Getting the context: [$ID]"
 
-        Get-Context -ID $ID -Vault $script:GitHub.ContextVault | ForEach-Object {
+        Get-Context -ID $ID -Vault $script:GitHub.ContextVault | Where-Object { $_.ID -ne $script:GitHub.DefaultConfig.ID } | ForEach-Object {
             $contextObj = $_
             Write-Verbose 'Context:'
             $contextObj | Select-Object * | Out-String -Stream | ForEach-Object { Write-Verbose $_ }
