3c91fb49d115c902d9c5ec806ab96787284152f1
Marius Storhaug <marstor@hotmail.com>
2025-06-18 22:56:28 +0200

    Add `Reset-GitHubOutput` function to clear GitHub output contents

---
+++ src/functions/public/Commands/Reset-GitHubOutput.ps1
@@ -0,0 +1,45 @@
+﻿function Reset-GitHubOutput {
+    <#
+        .SYNOPSIS
+        Resets the GitHub output.
+
+        .DESCRIPTION
+        Resets the GitHub output by clearing the contents of $env:GITHUB_OUTPUT.
+
+        .EXAMPLE
+        Reset-GitHubOutput
+
+        Resets the content in the GitHub output file.
+
+        .LINK
+        https://psmodule.io/GitHub/Functions/Commands/Reset-GitHubOutput
+    #>
+    [CmdletBinding()]
+    param(
+        # The path to the GitHub output file.
+        [Parameter()]
+        [string] $Path = $env:GITHUB_OUTPUT
+    )
+
+    begin {
+        $stackPath = Get-PSCallStackPath
+        Write-Debug "[$stackPath] - Start"
+    }
+
+    process {
+        if (-not $Path) {
+            throw 'The path to the GitHub output file is not set. Please set the path to the GitHub output file using the -Path parameter.'
+        }
+        Write-Debug "[$stackPath] - Output path"
+        Write-Debug $Path
+        if (-not (Test-Path -Path $Path)) {
+            throw "File not found: $Path"
+        }
+
+        '' | Set-Content -Path $Path -Force
+    }
+
+    end {
+        Write-Debug "[$stackPath] - End"
+    }
+}
