9307c0ef676dd950126605ea401a2df5de7267c5
Marius Storhaug <marstor@hotmail.com>
2025-06-27 15:13:44 +0200

    🩹 [Patch]: Update property names in GitHubConfig format files for consistency and clarity

--- src/functions/public/Config/Set-GitHubConfig.ps1
+++ src/functions/public/Config/Set-GitHubConfig.ps1
@@ -11,9 +11,13 @@
 
         Sets the value of DefaultUser to 'Octocat' in the GitHub module configuration.
 
+        .OUTPUTS
+        GitHubConfig
+
         .LINK
         https://psmodule.io/GitHub/Functions/Config/Set-GitHubConfig
     #>
+    [OutputType([GitHubConfig])]
     [CmdletBinding(SupportsShouldProcess)]
     param(
         # Set the access token type.
@@ -39,7 +43,10 @@
         Write-Verbose "Setting [$Name] to [$Value]"
         $script:GitHub.Config.$Name = $Value
         if ($PSCmdlet.ShouldProcess('ContextSetting', 'Set')) {
-            Set-Context -Context $script:GitHub.Config -Vault $script:GitHub.ContextVault -PassThru:$PassThru
+            $item = Set-Context -Context $script:GitHub.Config -Vault $script:GitHub.ContextVault -PassThru:$PassThru
+            if ($PassThru) {
+                [GitHubConfig]::new($item)
+            }
         }
     }
 
