e4aeb07e245caf60bcec3c680dad7510a789b165
Marius Storhaug <marstor@hotmail.com>
2025-06-27 14:20:46 +0200

    🩹 [Patch]: Update virtual terminal support checks to use EnvironmentType from GitHubConfig across multiple format files

--- src/functions/private/Auth/Context/Set-GitHubContext.ps1
+++ src/functions/private/Auth/Context/Set-GitHubContext.ps1
@@ -83,7 +83,7 @@
                             Write-Debug "Failed to get the GitHub App with the slug: [$($contextObj['Username'])]."
                         }
                     }
-                    if ($script:GitHub.EnvironmentType -eq 'GHA') {
+                    if ((Get-GitHubConfig).EnvironmentType -eq 'GHA') {
                         $gitHubEvent = Get-Content -Path $env:GITHUB_EVENT_PATH -Raw | ConvertFrom-Json
                         $installationType = $gitHubEvent.repository.owner.type
                         $installationName = $gitHubEvent.repository.owner.login
@@ -146,7 +146,7 @@
                 if ($Default) {
                     Switch-GitHubContext -Context $contextObj['Name']
                 }
-                if ($script:GitHub.EnvironmentType -eq 'GHA') {
+                if ((Get-GitHubConfig).EnvironmentType -eq 'GHA') {
                     if ($contextObj['AuthType'] -ne 'APP') {
                         Set-GitHubGitConfig -Context $contextObj['Name']
                         Connect-GitHubCli -Context $contextObj
