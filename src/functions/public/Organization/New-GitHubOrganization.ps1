62cb520dcf83e69f55e7d555d183828d8833b514
Marius Storhaug <marstor@hotmail.com>
2025-07-07 12:37:07 +0200

    🩹 [Patch]: Ensure context is resolved in New-GitHubOrganization function's begin block

--- src/functions/public/Organization/New-GitHubOrganization.ps1
+++ src/functions/public/Organization/New-GitHubOrganization.ps1
@@ -43,6 +43,7 @@
     begin {
         $stackPath = Get-PSCallStackPath
         Write-Debug "[$stackPath] - Start"
+        $Context = Resolve-GitHubContext -Context $Context
         Assert-GitHubContext -Context $Context -AuthType IAT, PAT, UAT
     }
 
