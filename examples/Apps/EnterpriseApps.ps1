f160e2d3f11cf41ad80bbdbed324c588da0eeed7
Marius Storhaug <marstor@hotmail.com>
2025-07-09 21:36:14 +0200

    🩹 [Patch]: Refactor EnterpriseApps.ps1 to include GitHub connection setup and fix variable casing

--- examples/Apps/EnterpriseApps.ps1
+++ examples/Apps/EnterpriseApps.ps1
@@ -1,11 +1,21 @@
-﻿$ClientIDs = @(
+﻿$ClientID = ''
+$PrivateKey = @'
+-----BEGIN RSA PRIVATE KEY-----
+
+-----END RSA PRIVATE KEY-----
+'@
+Connect-GitHub -ClientID $ClientID -PrivateKey $PrivateKey
+Connect-GitHubApp -Enterprise 'msx'
+
+# The apps you want to install on orgs in the enterprise
+$ClientIDs = @(
     'Iv1.f26b61bc99e69405'
 )
 $Enterprise = 'msx'
 $Organization = '*'
 
 $installableOrgs = Get-GitHubOrganization -Enterprise $Enterprise
-$orgs = $installableOrgs | Where-Object { $_.Name -like $organization }
+$orgs = $installableOrgs | Where-Object { $_.Name -like $Organization }
 foreach ($org in $orgs) {
     foreach ($ClientID in $ClientIDs) {
         Install-GitHubApp -Enterprise $Enterprise -Organization $org.Name -ClientID $ClientID -RepositorySelection all
