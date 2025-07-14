1d35aec1897ab9872ecb9b70fd3d76432432250a
Marius Storhaug <marstor@hotmail.com>
2025-07-14 20:37:50 +0200

    🩹 [Patch]: Simplify FormatColorByRatio method by removing unnecessary parameters and adding terminal support check

--- src/classes/public/GitHubFormatter.ps1
+++ src/classes/public/GitHubFormatter.ps1
@@ -1,28 +1,22 @@
 ﻿class GitHubFormatter {
-    static [string] FormatColorByRatio([double]$Ratio, [string]$Text, $HostObject, $PSStyleObject) {
-        if ($HostObject.UI.SupportsVirtualTerminal -and ($env:GITHUB_ACTIONS -ne 'true')) {
-            # Ensure ratio is between 0 and 1
-            $Ratio = [Math]::Min([Math]::Max($Ratio, 0), 1)
+    static [string] FormatColorByRatio([double]$Ratio, [string]$Text) {
+        $Ratio = [Math]::Min([Math]::Max($Ratio, 0), 1)
 
-            if ($Ratio -ge 1) {
-                $r = 0
-                $g = 255
-            } elseif ($Ratio -le 0) {
-                $r = 255
-                $g = 0
-            } elseif ($Ratio -ge 0.5) {
-                $r = [Math]::Round(255 * (2 - 2 * $Ratio))
-                $g = 255
-            } else {
-                $r = 255
-                $g = [Math]::Round(255 * (2 * $Ratio))
-            }
-
-            $color = $PSStyleObject.Foreground.FromRgb($r, $g, 0)
-            $reset = $PSStyleObject.Reset
-            return "$color$Text$reset"
+        if ($Ratio -ge 1) {
+            $r = 0
+            $g = 255
+        } elseif ($Ratio -le 0) {
+            $r = 255
+            $g = 0
+        } elseif ($Ratio -ge 0.5) {
+            $r = [Math]::Round(255 * (2 - 2 * $Ratio))
+            $g = 255
+        } else {
+            $r = 255
+            $g = [Math]::Round(255 * (2 * $Ratio))
         }
-
-        return $Text
+        $color = "`e[38;2;$r;$g;0m"
+        $reset = "`e[0m"
+        return "$color$Text$reset"
     }
 }
