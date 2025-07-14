a25410d769ba4877a3b88d5ce0e58e119c854fc5
Marius Storhaug <marstor@hotmail.com>
2025-07-14 16:54:23 +0200

    🩹 [Patch]: Update GitHub context formatting to use TokenExpiresIn and ResetsIn properties for better clarity and handling of expiration states

--- src/header.ps1
+++ src/header.ps1
@@ -1,3 +1,5 @@
-﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidLongLines', '', Justification = 'Contains long links.')]
+﻿#Requires -Modules @{ ModuleName = 'TimeSpan'; RequiredVersion = '3.0.0' }
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidLongLines', '', Justification = 'Contains long links.')]
 [CmdletBinding()]
 param()
