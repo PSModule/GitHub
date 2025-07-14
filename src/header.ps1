8ed4ff4682157de3a88ecbaea44ffad0586b3b4c
Marius Storhaug <marstor@hotmail.com>
2025-07-14 18:32:14 +0200

    🩹 [Patch]: Update TimeSpan module requirement to version 3.0.1 for compatibility

--- src/header.ps1
+++ src/header.ps1
@@ -1,4 +1,4 @@
-﻿#Requires -Modules @{ ModuleName = 'TimeSpan'; RequiredVersion = '3.0.0' }
+﻿#Requires -Modules @{ ModuleName = 'TimeSpan'; RequiredVersion = '3.0.1' }
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidLongLines', '', Justification = 'Contains long links.')]
 [CmdletBinding()]
