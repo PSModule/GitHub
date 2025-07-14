aa23533310ec2e8787287ce41e50448f29092e1e
Marius Storhaug <marstor@hotmail.com>
2025-07-14 22:46:08 +0200

    🩹 [Patch]: Update formatting of `GitHubContext` and `GitHubRateLimitResource` (#474)
    ## Description
    
    This pull request introduces updates to improve the handling and display
    of time-related properties in PowerShell scripts. The changes focus on
    replacing direct property access with the `Format-TimeSpan` function for
    better readability and consistency, while also adding color-coded visual
    indicators for certain time-related values.
    
    ### Time property updates:
    
    * 🪲 `src/formats/GitHubContext.Format.ps1xml`: Replaced `Remaining` with
    `TokenExpiresIn` for time calculations and display, and formatted the
    output using `Format-TimeSpan` for improved readability.
    
    * 🩹 `src/formats/GitHubRateLimitResource.Format.ps1xml`: Added a
    `ScriptBlock` to format the `ResetsIn` property using `Format-TimeSpan`.
    Introduced color-coded visual indicators for `ResetsIn` values based on
    their proximity to expiration, enhancing the user experience.
    
    ### Dependency update:
    
    * `src/header.ps1`: Added a `#Requires` directive for the `TimeSpan`
    module with version `3.0.0` to ensure compatibility with the new
    `Format-TimeSpan` function.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [x] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/classes/public/GitHubFormatter.ps1
@@ -0,0 +1,22 @@
+﻿class GitHubFormatter {
+    static [string] FormatColorByRatio([double]$Ratio, [string]$Text) {
+        $Ratio = [Math]::Min([Math]::Max($Ratio, 0), 1)
+
+        if ($Ratio -ge 1) {
+            $r = 0
+            $g = 255
+        } elseif ($Ratio -le 0) {
+            $r = 255
+            $g = 0
+        } elseif ($Ratio -ge 0.5) {
+            $r = [Math]::Round(255 * (2 - 2 * $Ratio))
+            $g = 255
+        } else {
+            $r = 255
+            $g = [Math]::Round(255 * (2 * $Ratio))
+        }
+        $color = "`e[38;2;$r;$g;0m"
+        $reset = "`e[0m"
+        return "$color$Text$reset"
+    }
+}
