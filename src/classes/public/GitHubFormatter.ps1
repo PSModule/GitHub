a6683d3ec126d610a5bd439448336313a9521256
Marius Storhaug <marstor@hotmail.com>
2025-07-15 10:59:52 +0200

    🩹 [Refactor]: Update GitHubReleaseAsset.Format.ps1xml to use FormatFileSize method for improved readability and consistency

--- src/classes/public/GitHubFormatter.ps1
+++ src/classes/public/GitHubFormatter.ps1
@@ -19,4 +19,14 @@
         $reset = "`e[0m"
         return "$color$Text$reset"
     }
+
+    static [string] FormatFileSize([long]$size) {
+        switch ($size) {
+            { $_ -ge 1TB } { return '{0:N2} TB' -f ($size / 1TB) }
+            { $_ -ge 1GB } { return '{0:N2} GB' -f ($size / 1GB) }
+            { $_ -ge 1MB } { return '{0:N2} MB' -f ($size / 1MB) }
+            { $_ -ge 1KB } { return '{0:N2} KB' -f ($size / 1KB) }
+        }
+        return "$size Bytes"
+    }
 }
