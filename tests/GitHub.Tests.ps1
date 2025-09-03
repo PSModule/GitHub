060360f1dd10a8d3abeb0c107ec9d673e7bb6f77
Marius Storhaug <marstor@hotmail.com>
2025-09-03 15:54:15 +0200

    Fix

--- tests/GitHub.Tests.ps1
+++ tests/GitHub.Tests.ps1
@@ -768,53 +768,6 @@ Describe 'API' {
     }
 }
 
-Describe 'Emojis' {
-    $authCases = . "$PSScriptRoot/Data/AuthCases.ps1"
-
-    Context 'As <Type> using <Case> on <Target>' -ForEach $authCases {
-        BeforeAll {
-            $context = Connect-GitHubAccount @connectParams -PassThru -Silent
-            LogGroup 'Context' {
-                Write-Host ($context | Format-List | Out-String)
-            }
-        }
-        AfterAll {
-            Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount -Silent
-            Write-Host ('-' * 60)
-        }
-
-        # Tests for APP goes here
-        if ($AuthType -eq 'APP') {
-            It 'Connect-GitHubApp - Connects as a GitHub App to <Owner>' {
-                $context = Connect-GitHubApp @connectAppParams -PassThru -Default -Silent
-                LogGroup 'Context' {
-                    Write-Host ($context | Format-List | Out-String)
-                }
-            }
-        }
-
-        # Tests for runners goes here
-        if ($Type -eq 'GitHub Actions') {}
-
-        # Tests for IAT UAT and PAT goes here
-        It 'Get-GitHubEmoji - Gets a list of all emojis' {
-            $emojis = Get-GitHubEmoji
-            LogGroup 'emojis' {
-                Write-Host ($emojis | Format-Table | Out-String)
-            }
-            $emojis | Should -Not -BeNullOrEmpty
-        }
-        It 'Get-GitHubEmoji - Downloads all emojis' {
-            Get-GitHubEmoji -Path $Home
-            LogGroup 'emojis' {
-                $emojis = Get-ChildItem -Path $Home -File
-                Write-Host ($emojis | Format-Table | Out-String)
-            }
-            $emojis | Should -Not -BeNullOrEmpty
-        }
-    }
-}
-
 Describe 'Webhooks' {
     BeforeAll {
         $secret = "It's a Secret to Everybody"
