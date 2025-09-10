f1a8fa8750c4f299d9ca4b21209fdfdcd50aa8df
Marius Storhaug <marstor@hotmail.com>
2025-09-10 16:05:52 +0200

    Remove argument completers for Connect-GitHubApp parameters

--- src/completers.ps1
+++ src/completers.ps1
@@ -2,8 +2,9 @@
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    $context = Resolve-GitHubContext -Context $fakeBoundParameter.Context
-    Get-GitHubAppInstallation -Context $context -Verbose:$false | Where-Object { $_.Type -eq 'User' -and $_.Target.Name -like "$wordToComplete*" } |
+    $context = Resolve-GitHubContext -Context $fakeBoundParameter.Context -Verbose:$false -Debug:$false
+    Get-GitHubAppInstallation -Context $context -Verbose:$false -Debug:$false |
+        Where-Object { $_.Type -eq 'User' -and $_.Target.Name -like "$wordToComplete*" } |
         ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_.Target.Name, $_.Target.Name, 'ParameterValue', $_.Target.Name)
         }
@@ -12,8 +13,9 @@ Register-ArgumentCompleter -CommandName Connect-GitHubApp -ParameterName Organiz
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    $context = Resolve-GitHubContext -Context $fakeBoundParameter.Context
-    Get-GitHubAppInstallation -Context $context -Verbose:$false | Where-Object { $_.Type -eq 'Organization' -and $_.Target.Name -like "$wordToComplete*" } |
+    $context = Resolve-GitHubContext -Context $fakeBoundParameter.Context -Verbose:$false -Debug:$false
+    Get-GitHubAppInstallation -Context $context -Verbose:$false -Debug:$false |
+        Where-Object { $_.Type -eq 'Organization' -and $_.Target.Name -like "$wordToComplete*" } |
         ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_.Target.Name, $_.Target.Name, 'ParameterValue', $_.Target.Name)
         }
@@ -22,8 +24,9 @@ Register-ArgumentCompleter -CommandName Connect-GitHubApp -ParameterName Enterpr
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter
 
-    $context = Resolve-GitHubContext -Context $fakeBoundParameter.Context
-    Get-GitHubAppInstallation -Context $context -Verbose:$false | Where-Object { $_.Type -eq 'Enterprise' -and $_.Target.Name -like "$wordToComplete*" } |
+    $context = Resolve-GitHubContext -Context $fakeBoundParameter.Context -Verbose:$false -Debug:$false
+    Get-GitHubAppInstallation -Context $context -Verbose:$false -Debug:$false |
+        Where-Object { $_.Type -eq 'Enterprise' -and $_.Target.Name -like "$wordToComplete*" } |
         ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_.Target.Name, $_.Target.Name, 'ParameterValue', $_.Target.Name)
         }
