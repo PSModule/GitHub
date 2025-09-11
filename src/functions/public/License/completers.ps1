f3ddda7b429f49d9b93d8050705b12014d13be65
Marius Storhaug <marstor@hotmail.com>
2025-09-11 10:35:26 +0200

    Refactor argument completers for GitHub license and organization commands to improve context handling and clarity

--- src/functions/public/License/completers.ps1
+++ src/functions/public/License/completers.ps1
@@ -1,11 +1,17 @@
 ﻿Register-ArgumentCompleter -CommandName Get-GitHubLicense -ParameterName Name -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
+
+    # Try to resolve an existing default context; if none exists fall back to anonymous requests.
+    $context = $fakeBoundParameters.Context ?? (Get-GitHubContext -ErrorAction SilentlyContinue -WarningAction SilentlyContinue)
+
     $params = @{
-        Context = $fakeBoundParameters.Context
-        Verbose = $false
-        Debug   = $false
+        Anonymous = ($null -eq $context) ? $true : $false
+        Context   = ($null -ne $context) ? $context : $null
+        Verbose   = $false
+        Debug     = $false
     }
+
     Get-GitHubLicense @params | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
     }
--- src/functions/public/Organization/completers.ps1
+++ src/functions/public/Organization/completers.ps1
@@ -15,7 +15,6 @@
 Register-ArgumentCompleter -CommandName ($script:PSModuleInfo.FunctionsToExport) -ParameterName Owner -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
-
     $params = @{
         Context = $fakeBoundParameters.Context
         Verbose = $false
