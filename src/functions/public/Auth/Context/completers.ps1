61a0ea424de3122a5a9e5f7699708a19f25e797c
Marius Storhaug <marstor@hotmail.com>
2025-09-11 09:44:26 +0200

    Refactor argument completers for Gitignore commands to improve context handling and completion results

--- src/functions/public/Auth/Context/completers.ps1
+++ src/functions/public/Auth/Context/completers.ps1
@@ -12,9 +12,9 @@
         $contexts += 'Anonymous'
     }
 
-    $contexts += (Get-GitHubContext -ListAvailable -Verbose:$false -Debug:$false).Name
-    $contexts = $contexts | Sort-Object -Unique
-    $contexts | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    $contexts += Get-GitHubContext -ListAvailable -Verbose:$false -Debug:$false
+    $contexts = $contexts | Sort-Object -Property Name
+    $contexts | Where-Object { $_.Name -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_.Name, $_.Name, 'ParameterValue', $_.Name)
     }
 }
--- src/functions/public/Gitignore/completers.ps1
+++ src/functions/public/Gitignore/completers.ps1
@@ -1,11 +1,17 @@
 ﻿Register-ArgumentCompleter -CommandName Get-GitHubGitignore -ParameterName Name -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
     $null = $commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters
+
+    # Try to resolve an existing default context; if none exists fall back to anonymous requests.
+    $context = $fakeBoundParameters.Context ?? (Get-GitHubContext -ErrorAction SilentlyContinue -WarningAction SilentlyContinue)
+
     $params = @{
-        Anonymous = $true
-        Verbose   = $false
-        Debug     = $false
+        Anonymous = ($null -eq $context) ? $true : $false
+        Context = ($null -ne $context) ? $context : $null
+        Verbose = $false
+        Debug   = $false
     }
+
     Get-GitHubGitignore @params | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
