76bca80ab38811488cc79dbae8a111b441fd0669
Marius Storhaug <marstor@hotmail.com>
2025-09-21 22:38:24 +0200

    🩹 [Patch]: Remove commented-out code and enable relevant test assertions for GitHub App connection

--- tests/Apps.Tests.ps1
+++ tests/Apps.Tests.ps1
@@ -38,7 +38,7 @@ Describe 'Apps' {
             Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount -Silent
             Write-Host ('-' * 60)
         }
-<#
+
         It 'Get-GitHubApp - Get an app by slug' -Skip:($AuthType -eq 'APP') {
             $app = Get-GitHubApp -Slug 'github-actions'
             LogGroup 'App by slug' {
@@ -182,7 +182,7 @@ Describe 'Apps' {
                 }
             }
         }
-#>
+
         Context 'Installation' -Skip:($AuthType -ne 'APP') {
             BeforeAll {
                 $githubApp = Get-GitHubApp
@@ -204,50 +204,50 @@ Describe 'Apps' {
             }
 
             It 'Connect-GitHubApp - Connects as a GitHub App to <Owner>' {
-                # $context | Should -BeOfType 'GitHubAppInstallationContext'
+                $context | Should -BeOfType 'GitHubAppInstallationContext'
                 $context.ClientID | Should -Be $githubApp.ClientID
                 $context.TokenExpiresAt | Should -BeOfType [datetime]
                 $context.InstallationID | Should -BeOfType [uint64]
                 $context.InstallationID | Should -BeGreaterThan 0
-                # $context.Events | Should -BeOfType 'string'
-                # $context.InstallationType | Should -Be $ownertype
-                # $context.InstallationName | Should -Be $owner
-                # $context.ID | Should -Be "$($config.HostName)/$($githubApp.Slug)/$ownertype/$owner"
-                # $context.Name | Should -Be "$($config.HostName)/$($githubApp.Slug)/$ownertype/$owner"
-                # $context.DisplayName | Should -Be $githubApp.Name
-                # $context.Type | Should -Be 'Installation'
-                # $context.HostName | Should -Be $config.HostName
-                # $context.ApiBaseUri | Should -Be $config.ApiBaseUri
-                # $context.ApiVersion | Should -Be $config.ApiVersion
-                # $context.AuthType | Should -Be 'IAT'
-                # $context.NodeID | Should -Not -BeNullOrEmpty
-                # $context.DatabaseID | Should -Not -BeNullOrEmpty
-                # $context.UserName | Should -Be $githubApp.Slug
-                # $context.Token | Should -BeOfType [System.Security.SecureString]
-                # $context.TokenType | Should -Be 'ghs'
-                # $context.HttpVersion | Should -Be $config.HttpVersion
-                # $context.PerPage | Should -Be $config.PerPage
-                # $context.Permissions.Count | Should -BeGreaterThan 0
-                # $context.Permissions | Should -BeOfType [GitHubPermission]
-                # $context.Permissions.Name | Should -BeIn $permissionsList.Name
-                # $context.Events | Should -BeOfType 'string'
+                $context.Events | Should -BeOfType 'string'
+                $context.InstallationType | Should -Be $ownertype
+                $context.InstallationName | Should -Be $owner
+                $context.ID | Should -Be "$($config.HostName)/$($githubApp.Slug)/$ownertype/$owner"
+                $context.Name | Should -Be "$($config.HostName)/$($githubApp.Slug)/$ownertype/$owner"
+                $context.DisplayName | Should -Be $githubApp.Name
+                $context.Type | Should -Be 'Installation'
+                $context.HostName | Should -Be $config.HostName
+                $context.ApiBaseUri | Should -Be $config.ApiBaseUri
+                $context.ApiVersion | Should -Be $config.ApiVersion
+                $context.AuthType | Should -Be 'IAT'
+                $context.NodeID | Should -Not -BeNullOrEmpty
+                $context.DatabaseID | Should -Not -BeNullOrEmpty
+                $context.UserName | Should -Be $githubApp.Slug
+                $context.Token | Should -BeOfType [System.Security.SecureString]
+                $context.TokenType | Should -Be 'ghs'
+                $context.HttpVersion | Should -Be $config.HttpVersion
+                $context.PerPage | Should -Be $config.PerPage
+                $context.Permissions.Count | Should -BeGreaterThan 0
+                $context.Permissions | Should -BeOfType [GitHubPermission]
+                $context.Permissions.Name | Should -BeIn $permissionsList.Name
+                $context.Events | Should -BeOfType 'string'
             }
 
-            # It 'Connect-GitHubApp - TokenExpiresIn property should be calculated correctly' {
-            #     $context.TokenExpiresIn | Should -BeOfType [TimeSpan]
-            #     $context.TokenExpiresIn.TotalMinutes | Should -BeGreaterThan 0
-            #     $context.TokenExpiresIn.TotalMinutes | Should -BeLessOrEqual 60
-            # }
+            It 'Connect-GitHubApp - TokenExpiresIn property should be calculated correctly' {
+                $context.TokenExpiresIn | Should -BeOfType [TimeSpan]
+                $context.TokenExpiresIn.TotalMinutes | Should -BeGreaterThan 0
+                $context.TokenExpiresIn.TotalMinutes | Should -BeLessOrEqual 60
+            }
 
-            # It 'Revoked GitHub App token should fail on API call' -Skip:($TokenType -eq 'GITHUB_TOKEN') {
-            #     $org = Get-GitHubOrganization -Name PSModule -Context $context
-            #     $org | Should -Not -BeNullOrEmpty
-            #     $context | Disconnect-GitHub
+            It 'Revoked GitHub App token should fail on API call' -Skip:($TokenType -eq 'GITHUB_TOKEN') {
+                $org = Get-GitHubOrganization -Name PSModule -Context $context
+                $org | Should -Not -BeNullOrEmpty
+                $context | Disconnect-GitHub
 
-            #     {
-            #         Invoke-RestMethod -Method Get -Uri "$($context.ApiBaseUri)/orgs/PSModule" -Authentication Bearer -Token $context.token
-            #     } | Should -Throw
-            # }
+                {
+                    Invoke-RestMethod -Method Get -Uri "$($context.ApiBaseUri)/orgs/PSModule" -Authentication Bearer -Token $context.token
+                } | Should -Throw
+            }
         }
     }
 }
