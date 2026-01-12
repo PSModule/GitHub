8867fe7584a949c648c4c8a96b239de725e11fa5
Marius Storhaug <marstor@hotmail.com>
2026-01-12 22:34:47 +0100

    Add initial Pester tests for GitHub API interactions
    - Created TEMPLATE.ps1 for structuring tests with Pester.
    - Implemented Teams.Tests.ps1 to test GitHub Teams API functionalities including team creation, retrieval, updating, and deletion.
    - Developed Users.Tests.ps1 to validate user-related API calls, including user retrieval and updates.
    - Added Variables.Tests.ps1 to test GitHub variable management across different scopes (organization, repository, environment).
    - Included necessary suppressions for Pester warnings and established logging for better output visibility during test execution.

--- tests/AfterAll.ps1
+++ tests/AfterAll.ps1
@@ -2,5 +2,16 @@
 param()
 
 LogGroup 'AfterAll - Global Test Teardown' {
-    
+
+
+    switch ($OwnerType) {
+        'user' {
+            Get-GitHubRepository | Where-Object { $_.Name -like "$repoPrefix*" } | Remove-GitHubRepository -Confirm:$false
+        }
+        'organization' {
+            Get-GitHubRepository -Organization $Owner | Where-Object { $_.Name -like "$repoPrefix*" } | Remove-GitHubRepository -Confirm:$false
+        }
+    }
+    Get-GitHubContext -ListAvailable | Disconnect-GitHubAccount -Silent
+    Write-Host ('-' * 60)
 }
--- tests/BeforeAll.ps1
+++ tests/BeforeAll.ps1
@@ -2,5 +2,41 @@
 param()
 
 LogGroup 'BeforeAll - Global Test Setup' {
+    $authCases = . "$PSScriptRoot/Data/AuthCases.ps1"
 
+    $prefix = 'Test'
+    $os = $env:RUNNER_OS
+    $id = $env:GITHUB_RUN_ID
+
+    foreach ($authCase in $authCases) {
+        $authCase.GetEnumerator() | ForEach-Object { Set-Variable -Name $_.Key -Value $_.Value }
+
+        LogGroup 'Repository setup' {
+            $context = Connect-GitHubAccount @connectParams -PassThru -Silent
+            Write-Host ($context | Out-String)
+            if ($AuthType -eq 'APP') {
+                $context = Connect-GitHubApp @connectAppParams -PassThru -Default -Silent
+                Write-Host ($context | Format-List | Out-String)
+            }
+
+            $repoPrefix = "$prefix-$os-$TokenType"
+            $repoName = "$repoPrefix-$id"
+
+            switch ($OwnerType) {
+                'user' {
+                    Get-GitHubRepository | Where-Object { $_.Name -like "$repoPrefix*" } | Remove-GitHubRepository -Confirm:$false
+                    New-GitHubRepository -Name $repoName -Confirm:$false
+                }
+                'organization' {
+                    Get-GitHubRepository -Organization $Owner | Where-Object { $_.Name -like "$repoPrefix*" } | Remove-GitHubRepository -Confirm:$false
+                    New-GitHubRepository -Organization $Owner -Name $repoName -Confirm:$false
+                }
+            }
+        }
+        LogGroup 'Environment setup' {
+            $environmentName = "$prefix-$os-$TokenType-$id"
+        }
+        LogGroup 'Variables setup' {}
+        LogGroup 'Secrets setup' {}
+    }
 }
