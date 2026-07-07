158a08c8c06d04d644e042741cd298cfc573a351
Marius Storhaug <marstor@hotmail.com>
2026-07-07 16:40:25 +0200

    Lock Pester test dependency to the 6.x major version

--- tests/Artifacts.Tests.ps1
+++ tests/Artifacts.Tests.ps1
@@ -1,4 +1,4 @@
-﻿#Requires -Modules @{ ModuleName = 'Pester'; RequiredVersion = '5.8.0'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
+﻿#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '6.0.0'; MaximumVersion = '6.999.999'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
     'PSUseDeclaredVarsMoreThanAssignments', '',
